e8e896fe256622f9c11ea11bf6b4667f2ef4624e
yusuf <myusufsb252015@gmail.com>
2022-06-13 19:08:53 +0300

    append

---
+++ Java Script/DERS NOTLARI/JS-21.04 DERSİ/app.js
@@ -0,0 +1,81 @@
+// // this code show the app is running
+
+// console.log("app.js is running");
+
+// let ad = prompt('Adınızı girin');
+
+// // let sonuc = confirm('Emin misiniz');
+
+// // console.log(sonuc);
+
+
+// // console.log("Adınız: ",ad);
+// // console.log("Adınız: " + ad); // ikinci yöntem
+
+// // console.log("I'm happy");
+
+// // console.log('"Istanbul" \n <br> is the economic capital');
+
+// //document.write('"Istanbul" \n <br> is the economic capital');
+// // yaparsak sayfaya yazdırır mor olan yere soltaraftaki ekrana
+
+// console.log("adiniz : "+ ad + " dur ");
+// console.log( `adiniz : ${ad} dur `);     // bu şekilde de oluyor $ ile {} bunlar sayesinde
+
+var myNumber1 = 5;
+console.log(myNumber1);
+
+/*
+
+
+
+
+
+
+*/
+
+var myNumber1 = 'merhaba';
+console.log(myNumber1);
+    
+let var1 = 55;
+console.log(var1);
+
+var1 = 99;
+console.log(var1);
+
+{
+    let localVar1 = 'Local variable';
+    var myName = 'yusuf';
+
+    console.log(localVar1);
+
+}
+
+// console.log(localVar1);
+console.log(myName);
+
+// ! const konusu
+
+const student = {
+
+    firstName: 'yusuf',
+    number : 'F1689'
+
+
+}
+
+const otherStudent = student;
+console.log(otherStudent);
+
+otherStudent.number = 'F1597';
+
+console.log(student);
+
+// otherStudent = {
+
+//     firstName: 'Emre',        // !!!!!!!!!bu be satır hata veriyor!!!!!
+//     number: 'F1596',
+
+// };
+
+// console.log(otherStudent);
