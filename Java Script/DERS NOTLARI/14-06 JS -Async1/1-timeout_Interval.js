95668def396b3d63239a2532aa946fdc1466b1ff
yusuf <myusufsb252015@gmail.com>
2022-06-14 21:53:50 +0300

    append

--- Java Script/DERS NOTLARI/14-06 JS -Async1/1-timeout_Interval.js
+++ Java Script/DERS NOTLARI/14-06 JS -Async1/1-timeout_Interval.js
@@ -19,4 +19,93 @@
 //* Senkron
 //* ---------------------------------------------------------------
 
-console.log('TIMEOUT');
+// console.log('TIMEOUT');
+
+// const bekle = (ms) => {
+//     const start = new Date().getTime();
+//     while (new Date().getTime() < start + ms);
+// };
+
+
+// console.log('hello');
+// // alert('ITF');  //! blocking-code
+// console.time('timer');
+// bekle(3000)
+// console.timeEnd('timer');
+// console.log('FS11');
+
+
+//  Asenkron (setTimeout)
+// ----------------------------------------
+
+// macro task kuyruğu
+
+// setTimeout(() => {
+//     // non-blocking code
+//     console.log('Selamin Aleyküm');
+// }, 1000);
+
+
+// // micro task kuyruğu
+// console.log('Aleyküm Selam');
+// console.log('Merhaba');
+
+// setTimeout(() => {
+//     // non-blocking code
+//     console.log('Dostum Naber?');
+// }, 500);
+
+//  Asenkron (setInterval, clearInterval)
+//----------------------------------------------------
+// console.log
+// let counter = 0;
+
+// setInterval(() => {
+//     console.log(++counter);
+
+// }, 10000);
+
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakta bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+setTimeout(() => {
+    console.log("john:Hi");
+    setTimeout(() => {
+        console.log("Sarah: Hello");
+        setTimeout(() => {
+            console.log("John: How Are you?");
+            setTimeout(() => {
+                console.log("Fine and you?");
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+//? COZUMLER:
+//?----------------------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Java Script/DERS NOTLARI/14-06 JS -Async1/2-promise.js
+++ Java Script/DERS NOTLARI/14-06 JS -Async1/2-promise.js
@@ -29,3 +29,77 @@
 //! Bu durumlar then() ve catch() metotlari ile yakalanabilir.
 //? then() ve catch() metotlari promise dondururler.
 //? Zincirleme olarak kullanilabilirler.
+
+const myPromise = new Promise((resolve, reject) => {
+    let success = Math.floor(Math.random() * 5);
+    const data = { a: 1, b: 2 };
+    if (success) {
+        resolve(data);
+        console.log('Task performed successfly');
+    } else {
+        reject(new error('Task Failed'));
+    }
+});
+
+console.log(myPromise);
+
+myPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+
+//* Example-2: TEA TIME
+//* -----------------------------------------------------------------------
+//* 1. wait until water boiled -> kettle -> work or failed
+//* 2. add tea --> tea is avalaible or not
+//* 3. wait for brewing
+const wait = (ms) => {
+    const start = new Date().getTime();
+    while (new Date().getTime() < start + ms);
+};
+const brewTea = () => {
+    boil()
+        .then((status1) => {
+            console.log(status1);
+            return addTea();
+        })
+        .then((status2) => {
+            console.log(status2);
+            wait(1000);
+            return "Tea is ready. Bon Appetite";
+        })
+        .then((status3) => {
+            console.log(status3);
+        })
+        .catch((err) => console.log(err));
+};
+const boil = () => {
+    return new Promise((resolve, reject) => {
+        const boiled = Math.floor(Math.random() * 5);
+        if (boiled) {
+            wait(1000);
+            resolve("Water Boiled");
+        } else {
+            reject(new Error("Kettle Failed"));
+        }
+    });
+};
+
+const addTea = () => {
+    return new Promise((resolve, reject) => {
+        const teaAvalaible = Math.floor(Math.random() * 3);
+        if (teaAvalaible) {
+            wait(1000);
+            resolve("Tea was added");
+        } else {
+            reject(new Error("Tea unavailable"));
+        }
+    });
+};
+
+brewTea();
+
+
+
+
+
+
+
+
