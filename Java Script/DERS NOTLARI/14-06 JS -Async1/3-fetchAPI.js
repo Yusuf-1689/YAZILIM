70c9e1a930dccec8c5bf20904247df4580d44133
yusuf <myusufsb252015@gmail.com>
2022-06-15 18:54:24 +0300

    append

--- Java Script/DERS NOTLARI/14-06 JS -Async1/3-fetchAPI.js
+++ Java Script/DERS NOTLARI/14-06 JS -Async1/3-fetchAPI.js
@@ -13,8 +13,9 @@
 const userDiv = document.querySelector('.users')
 
 fetch('https://api.github.com/users')
-    .then((data) => {
-        if (!resizeBy.ok) {
+    .then((res) => {
+        //! error handling
+        if (!res.ok) {
             throw new Error(`Something went wrong: ${res.status}`);
         }
         return res.json();
@@ -22,24 +23,17 @@ fetch('https://api.github.com/users')
     .then((data) => updateDom(data))
     .catch((err) => console.log(err));
 
-const updateDom = (date) => {
+const updateDom = (data) => {
     const userDiv = document.querySelector('.users');
 
     data.forEach((user) => {
         const { login, avatar_url, html_url } = user;
         userDiv.innerHTML += `
-            <h2 class="text-warning">NAME:${login}</h2>
-            <img src=${avatar_url} alt="">
-             `;
-    }
-
-
-
-
-
-
-
-
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />;
+  `;
+    });
+};
 
 
 
