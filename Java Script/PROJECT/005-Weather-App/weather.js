87bfbd56dd1774c12e9c2b215f3acfc36da4e17c
yusuf <myusufsb252015@gmail.com>
2022-06-16 22:21:31 +0300

    append

--- Java Script/PROJECT/005-Weather-App/weather.js
+++ Java Script/PROJECT/005-Weather-App/weather.js
@@ -1,49 +1,53 @@
-
-
 const form = document.querySelector("section.top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-form.addEventListener("submit", () => {
-    e.preventdefault();
+// localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     getWeatherDataFromApi();
 });
 
 // function getWeatherDataFromApi(){}
 const getWeatherDataFromApi = async () => {
     // alert("http request gone");
-    // input.value = "";    //aynısı form.reset ile
+    // input.value = "";
     let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
     // console.log(apikey);
     let inputVal = input.value;
     let unitType = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}&lang=${lang}
-    `;
-
+    let lang = "tr";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}&lang=${lang}`;
 
     try {
-        // const response = await fetch(url).then(response => response.json());
-        // axios.get(url) == axios(url)
+        // const response = await fetch.get(url).then(response => response.json());
+        //axios.get(url) == axios(url)
         const response = await axios(url);
+        const { name, main, sys, weather } = response.data;
         console.log(response.data);
-    } catch (error) {
+        let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+        const createdLi = document.createElement("li");
+        createdLi.classList.add("city");
+        const createdLiInnerHTML =
+            `<h2 class="city-name" data-name="${name}, ${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrl}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`;
+        createdLi.innerHTML = createdLiInnerHTML;
+        //append vs. prepend
+        list.prepend(createdLi);
+
+    }
+    catch (error) {
 
     }
     form.reset();
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
