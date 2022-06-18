b21b322fdf84b71d8101ef826a6638c5c8930f1d
yusuf <myusufsb252015@gmail.com>
2022-06-18 13:03:22 +0300

    append

--- Java Script/DERS NOTLARI/18-06 JS-OOP1/2-objectConstructor.js
+++ Java Script/DERS NOTLARI/18-06 JS-OOP1/2-objectConstructor.js
@@ -24,31 +24,65 @@ function Book(title, author, year) {
   //? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
   //? her bir instance'a da hayat bulmus olur.
   
+  //? instance
   const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
   const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
   
   console.log(book1);
   console.log(book2.getSummary());
-
+  
   book1.price = 100;
-
   console.log(book1, book2);
-
-  // Eğer yeni bir property veya fonksiyonu constructor'a eklemek istersek
-// prototype'ı kullanabilir
-
-  Book.prototype.getAge = function (){
+  
+  //! Eger yeni bir property veya fonksiyonu constructor'a eklemek istersek
+  //! prototype'ı kullanabiliriz.
+  
+  Book.prototype.getAge = function () {
     return new Date().getFullYear() - this.year;
   };
-
+  
   Book.prototype.type = 'novel';
-
+  
   console.log(book1.getAge());
   console.log(book2.getAge());
   console.log(book2.type);
+  
+  
+  //! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
+  //! sekilde metotlar tanimlamaya izin vermektedir.
+  //! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
+  //! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
+  //! ulasabilmesidir.
+  
+  //* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+  //* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
+  
+  //* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+  //* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+  
+  
+  
+  //!Object'lerin proto alanini gormk icin .protoype
+  console.log(Book.prototype);
+  
+  //!instance'larin proto alanini gormk icin .__proto__
+  console.log(book1.__proto__);
 
+  // ------------- INHERITIANCE (Kalitim - ES5) ----------
 
- console.log(Book.prototype);
+  function Magazine(title, author,year, month) {
+    //**-- parent'in object constructorinin cagrilmasi
+    Book.call(this,title,author,year)
+    this.month = month;
+  }
 
- 
- console.log(book1.__proto__);
+  const mag1 = new Magazine('Scientific Research', 'Einstein', 1905, 'September');
+
+  console.log(mag1);
+
+  //**  Prototipler doğrudan miras olarak gelmez
+  console.log(Magazine.prototype);
+
+  Magazine.prototype = Object.create(Book.prototype);
+  console.log(Magazine.prototype);
+  console.log(mag1.getAge());
