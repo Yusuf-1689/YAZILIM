df7c5da95760c748eb2e9e442e1c59919788754d
yusuf <myusufsb252015@gmail.com>
2022-06-22 22:56:16 +0300

    props

--- React/rc02_props/src/App.js
+++ React/rc02_props/src/App.js
@@ -1,8 +1,23 @@
+import Person from './Person';
 
 function App() {
   return (
     <>
-      <h1>APP</h1>
+      <Person
+        name="Canan Bayram"
+        img="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+        tel="5555 555555"
+      />
+      <Person
+        name="Can Bayram"
+        img="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445__340.jpg"
+        tel="5555 554444"
+      />
+      <Person
+        name="Ahmet Fevzi"
+        img="https://cdn.pixabay.com/photo/2015/01/27/09/58/man-613601__340.jpg"
+        tel="5555222222"
+      />
     </>
   );
 }
