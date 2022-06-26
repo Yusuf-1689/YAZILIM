97bd79718a2dc906c299d5370375a2776ccd725e
yusuf <myusufsb252015@gmail.com>
2022-06-27 00:06:00 +0300

    app-js

--- Java Script/DERS NOTLARI/Stop-Watch/app.js
+++ Java Script/DERS NOTLARI/Stop-Watch/app.js
@@ -1,55 +1,46 @@
-
 const startButton = document.getElementById("startTimer");
 const stopButton = document.querySelector("#resetTimer");
-let displayTimer = document.querySelector(".timerdisplay");
+let displayTimer = document.querySelector(".timerDisplay");
 let intervalId;
-let [ms,sec,min] = [0,0,0];
-// console.log(startButton,startButton,displayTimer);
+let [min,sec,ms] = [0,0,0];
 
-document.querySelector(".container").addEventListener("click", (e) =>{
-    if(e.target.classList.contains("start")){
-        if(startButton.classList.contains("start")){
-            startButton.classList.remove("start");
-            startButton.innerHTML = `<i class="fa-solid fa-pause"></i>`
-            intervalId = setInterval(timer , 10);
-        }
-        else{
-            startButton.classList.add("start");
-            startButton.innerHTML = `<i class="fa-solid start fa-play"></i>`
-            clearInterval(intervalId);
-        }
-    }
-    else if(e.target.classList.contains("stp")){
-        clearInterval(intervalId);
-        startButton.innerHTML = `<i class="fa-solid start fa-play"></i>`
-
-    }
-
+document.querySelector(".container").addEventListener("click", (e) => {
+ if(e.target.classList.contains("start")){
+  if(startButton.classList.contains("start")){
+   startButton.classList.remove("start");
+   startButton.innerHTML = `<i class="fa-solid start fa-pause"></i>`
+   intervalId = setInterval(timer , 10);
+  }
+  else {
+   startButton.classList.add("start");
+   startButton.innerHTML = `<i class="fa-solid start fa-play"></i>`
+   clearInterval(intervalId);
+  }
+ }
+ else if (e.target.classList.contains("stp")){
+  clearInterval(intervalId);
+  startButton.innerHTML = `<i class="fa-solid start fa-play"></i>`
+  displayTimer.innerHTML = `00 : 00 : 00`;
+  [min,sec,ms] = [0,0,0];
+  startButton.classList.add("start");
+ }
 });
 
-function timer() {
-    ms += 1;
-    if(ms == 100){
-        sec +=1;
-        ms = 0;
-    }
-    if(sec == 60){
-        min +=1;
-        sec = 0;
-    }
-    if(min == 60){
-        min = 0;
-    }
-    let msS = ms<10 ? "0"+ms : ms;
-    let secS = sec<10 ? "0"+sec : sec;
-    let minS = min<10 ? "0"+min : min;
-    displayTimer.innerHTML = `${minS} : ${secS} : ${msS}`;
-    // console.log(`${minS} : ${secS} : ${msS}`);
+function timer(){
+ ms += 1;
+ if (ms == 100){
+  sec += 1;
+  ms = 0;
+ }
+ if(sec == 60){
+  min += 1;
+  sec = 0;
+ }
+ if(min == 60){
+  min = 0;
+ }
+ let msS = ms<10 ? "0"+ms : ms;
+ let secS = sec<10 ? "0"+sec : sec;
+ let minS = min<10 ? "0"+min : min;
+ displayTimer.innerHTML = `${minS} : ${secS} : ${msS}`;
 }
-
-
-
-
-
-
-
