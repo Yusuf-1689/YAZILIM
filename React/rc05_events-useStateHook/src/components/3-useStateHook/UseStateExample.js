7a9de524e131df6ac0def42ac7d6d6bfb829866d
yusuf <myusufsb252015@gmail.com>
2022-06-27 23:05:17 +0300

    finished

--- React/rc05_events-useStateHook/src/components/3-useStateHook/UseStateExample.js
+++ React/rc05_events-useStateHook/src/components/3-useStateHook/UseStateExample.js
@@ -1,6 +1,4 @@
-
-
-//* ======================= HOOKS ==============================
+//* ========================== HOOKS ====================================
 //! Hook'lar fonksiyonel component'ler icerisinde state'leri kullanmamiza
 //! olanak saglayan ozel fonksiyonlardir.
 //! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler'in
@@ -18,12 +16,67 @@
 //*    Normal Javascript fonksiyonlari icerisinde cagrilmamalidir
 //*    (Custom hook'lar icerisinde bir hook cagrilabilir)
 //?    https://reactjs.org/docs/hooks-rules.html
-//* ==============================================================
+//* =====================================================================
 
-import { useState } from "react";
+import { useState } from 'react';
 
 const UseStateExample = () => {
-    return <div>UseStateExample</div>;
+  //* useState en cok kullanilan Hook'tur.
+  //* Bir state'in degisken, dizi ve obje ile kullanilabilmesine olanak saglar
+  //? useState hook'u 2 deger dondurur.
+  //?  1.si state degiskenidir.
+  //?  2.si ise state'i degistirmeye izin veren bir setter metodudur.
+  //? useState parametre olarak state'in ilk degerini alir.
+  const [count, setCount] = useState(0); //! array destruc.
+
+  const [info, setInfo] = useState({
+    name: 'Ahmet Yilmaz',
+    email: 'ay@gmail.com',
+    age: 32,
+  });
+
+  const inc = () => {
+    setCount(count + 1);
   };
-  
-  export default UseStateExample;
+
+  const dec = () => {
+    setCount(count - 1);
+  };
+
+  const incAge = () => {
+    // setInfo(info.age + 1);
+    // console.log(info.age);
+
+    // setInfo({ name: 'mehmet yilmaz', email:'my@gmail.com', age: 44});
+    setInfo({...info, age: info.age + 1 });
+  };
+
+  console.log(info);
+
+  return (
+    <div className="container text-center mt-4">
+      <section>
+        <h1 className="display-4 text-danger m-4">USESTATE COUNT:{count}</h1>
+        <button onClick={inc} className="btn btn-success">
+          INC
+        </button>
+        <button onClick={() => setCount(0)} className="btn btn-dark">
+          CLR
+        </button>
+        <button onClick={dec} className="btn btn-warning">
+          DEC
+        </button>
+      </section>
+
+      <section>
+        <h1 className="display-4 text-danger m-4">USESTATE OBJECT</h1>
+        <h2>{info.name}</h2>
+        <h3>{info.email}</h3>
+        <h3>{info.age}</h3>
+        <button onClick={incAge} className="btn btn-info">inc age</button>
+      </section>
+    </div>
+  );
+};
+
+export default UseStateExample;
