2224f55902240b6a44c85ef37abb2b36cb7d2945
yusuf <myusufsb252015@gmail.com>
2022-06-27 20:38:44 +0300

    click

---
+++ React/rc05_events-useStateHook/src/App.js
@@ -0,0 +1,13 @@
+import Events from "./components/1-event-intro/Events";
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Events/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/rc05_events-useStateHook/src/components/1-event-intro/Events.jsx
@@ -0,0 +1,27 @@
+// * =======================================================
+// *                     EVENTS
+// * =======================================================
+
+//? ReactJS, Tarayicilar arasi uyumluluk ve performans artisi gibi
+//? sebeplerden oturu Sentetik Event olarak adilandirilan Olaylari
+//? kullanir. Sentetik Event, aslinda tarayicinin dogal event'larinin
+//? bir sarmalayici (Wrapper) arabirimle ortulmesi ile olusur. Bu sayede,
+//? React ortaminda kullanilan event'larin bilindik tarayicilarda
+//? sorunsuz calismasini saglanir.
+//? Ayrinti icin : https://reactjs.org/docs/events.html
+
+const Events = () => {
+  const handleClick = () => {
+    alert('Btn Clicked');
+  };
+
+  return (
+    <div className="container text-center mt-4">
+      <button onClick={handleClick} className="btn btn-warning">
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default Events;
