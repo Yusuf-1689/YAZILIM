c877a433cda04d0e1e58331328577b29b2fc858f
yusuf <myusufsb252015@gmail.com>
2022-06-28 22:28:01 +0300

    key

--- React/rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ React/rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -1,9 +1,52 @@
 
 
-const KeyboardClipboard = () => {
-  return (
-    <div>KeyboardClipboard</div>
-  )
-}
+import { useState } from 'react';
 
-export default KeyboardClipboard
+const KeyboardClipboard = () => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (e) => {
+    // console.log(e.keyCode);
+    e.target.value = e.target.value.toUpperCase();
+    e.keyCode === 13 && alert('Entera basildi');
+
+  };
+
+  const handleAreaPaste = (e)=> {
+
+    e.target.value += e.clipboardData.getData('text').value.toLowerCase();
+    e.preventDefault();
+    
+  };
+
+  return (
+
+    <div className="container text-center">
+
+      <h2>CLIPBOARD EVENTS </h2>
+
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+
+      />
+
+      <p className="text-start m-4">{inputValue}</p>
+
+      <textarea 
+      name='area' 
+      id='area' 
+      cols="50"  
+      rows="10" 
+      onPaste={handleAreaPaste} 
+
+      ></textarea>
+
+    </div>
+  );
+};
+
+export default KeyboardClipboard;
