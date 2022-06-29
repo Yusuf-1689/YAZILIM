2abe3010630d21e67620fd16f3e3e474a23ea690
yusuf <myusufsb252015@gmail.com>
2022-06-29 18:21:37 +0300

    finished

--- React/rc06_forms_otherEvents/src/App.js
+++ React/rc06_forms_otherEvents/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      {/* <FormObject/> */}
-     {/* <KeyboardEvents /> */}
-      {/* <MouseEvent />  */}
+      {/* <FormObject /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
     </div>
   );
 }
