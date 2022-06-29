1fee60873f975f442c5d1ee9a10f50949c087e0f
yusuf <myusufsb252015@gmail.com>
2022-06-29 20:57:13 +0300

    card

--- React/rc07_project/src/components/card/Card.js
+++ React/rc07_project/src/components/card/Card.js
@@ -1,1 +1,17 @@
-import React from 'react';
+import { languages } from '../../helpers/data';
+
+const Card = () => {
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="language-title">Languages</h1>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- React/rc07_project/src/components/header/Header.js
+++ React/rc07_project/src/components/header/Header.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import reactLogo from '../../assets/react.svg';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="logo-container">
+      <img className="react-logo" src={reactLogo} alt="react-logo" />
+    </div>
+  );
+};
+
+export default Header;
