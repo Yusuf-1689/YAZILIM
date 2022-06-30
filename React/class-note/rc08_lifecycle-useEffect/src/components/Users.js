ec814b2e8edce956151aa668fa65bb90938f33e0
yusuf <myusufsb252015@gmail.com>
2022-06-30 23:07:31 +0300

    finished

--- React/class-note/rc08_lifecycle-useEffect/src/components/Users.js
+++ React/class-note/rc08_lifecycle-useEffect/src/components/Users.js
@@ -1,22 +1,34 @@
-
-
+import { useState, useEffect } from 'react';
 
 const Users = () => {
-    fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
-      console.log(res.json())
-    );
-  
-    return (
-      <div>
-        <h1>USER LIST</h1>
-        <div className="row">
-          <div className="col">
-            <img src="" alt="" />
-            <h6>Name</h6>
-          </div>
+  const [users, setUsers] = useState("");
+
+  const getUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  };
+
+  useEffect(() => {
+    //?componentDidMount
+    getUsers();
+  }, []);
+
+  console.log(users);
+  return (
+    <div>
+      <h1>USER LIST</h1>
+      {/* <button className="btn btn-success" onClick={getUsers}>
+        Get users
+      </button> */}
+      <div className="row">
+        <div className="col">
+          <img src="" alt="" />
+          <h6>Name</h6>
         </div>
       </div>
-    );
-  };
-  
-  export default Users;
+    </div>
+  );
+};
+
+export default Users;
