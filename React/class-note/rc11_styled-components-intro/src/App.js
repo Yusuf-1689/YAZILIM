70cedd5a07f71912bfbb222afd9fdc11fe09dc7a
yusuf <myusufsb252015@gmail.com>
2022-07-05 21:10:32 +0300

    append

---
+++ React/class-note/rc11_styled-components-intro/src/App.js
@@ -0,0 +1,25 @@
+import Button, { TomatoButton } from './components/Button.styled';
+import Container from './components/Container.styled';
+import HeaderText from './components/Header.styled';
+import StyledLink from './components/Link.styled';
+
+const App = () => {
+  return (
+    <>
+      <Container bg="green">
+        <HeaderText>STYLED COMPONENTS</HeaderText>
+        <Button primary>Click</Button>
+        <Button>Click</Button>
+        <TomatoButton>Click</TomatoButton>
+        <TomatoButton primary>Click</TomatoButton>
+        <HeaderText color="white">STYLED COMPONENTS</HeaderText>
+
+        <StyledLink href="https://clarusway.com/" target="_blank">
+          Clarusway
+        </StyledLink>
+      </Container>
+    </>
+  );
+};
+
+export default App;
---
+++ React/class-note/rc11_styled-components-intro/src/components/Link.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  font-size: 1rem;
+  text-decoration: none;
+  color: white;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+export default StyledLink;
