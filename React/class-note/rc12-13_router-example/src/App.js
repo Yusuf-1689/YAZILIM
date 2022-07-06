0d0642aac0c9d0eb2e466677ddaf27dc83b289fc
yusuf <myusufsb252015@gmail.com>
2022-07-06 21:50:28 +0300

    nav

--- React/class-note/rc12-13_router-example/src/App.js
+++ React/class-note/rc12-13_router-example/src/App.js
@@ -1,19 +1,25 @@
 import Footer from './components/Footer';
-import MyNavbar from './components/Nav';
+import Nav from './components/Nav';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Instructors from './pages/Instructors';
 import NotFound from './pages/NotFound';
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <>
-      <MyNavbar />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+    <BrowserRouter>
+    <Nav />
+    <Routes>
+    <Route path="/" element={<Home />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
+      
       <Footer />
+    </BrowserRouter>
+     
     </>
   );
 }
--- React/class-note/rc12-13_router-example/src/components/Nav.jsx
+++ React/class-note/rc12-13_router-example/src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import logo from '../img/logo.png';
-
+import { Link, NavLink } from 'react-router-dom';
 function Nav() {
   return (
     <nav className="navbar navbar-expand-md navbar-light">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           <img src={logo} alt="" />
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -18,18 +18,36 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
             <li className="nav-item">
-              <a href="/" className="nav-link active" aria-current="page">
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && 'red' })}
+                to="/"
+                className="nav-link active"
+                aria-current="page"
+              >
                 Home
-              </a>
+              </NavLink>
             </li>
 
-            <a href="/instructors" className="nav-link">
-              Instructors
-            </a>
             <li className="nav-item">
-              <a href="/contact" className="nav-link">
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && 'red' })}
+                to="/instructors"
+                className="nav-link"
+                aria-current="page"
+              >
+                Instructors
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                style={({ isActive }) => ({ color: isActive && 'red' })}
+                to="/contact"
+                className="nav-link"
+                aria-current="page"
+              >
                 Contact
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
