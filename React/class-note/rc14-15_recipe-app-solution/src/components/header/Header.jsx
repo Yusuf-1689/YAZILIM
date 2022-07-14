133f7dc93373e7e0e55dd43293f7671debff835d
yusuf <myusufsb252015@gmail.com>
2022-07-14 21:08:05 +0300

    header

--- React/class-note/rc14-15_recipe-app-solution/src/components/header/Header.jsx
+++ React/class-note/rc14-15_recipe-app-solution/src/components/header/Header.jsx
@@ -1,7 +1,41 @@
-import HeaderStyle from './Header.style';
+import {
+  HeaderContainer,
+  MainHeader,
+  FormContainer,
+  FoodInput,
+  Button,
+  Select,
+} from './Header.style';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ setQuery, setSelectedMeal, mealType, getData }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+  return (
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
+      <FormContainer onSubmit={handleSubmit}>
+        <FoodInput
+          type="text"
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button type="submit">SEARCH</Button>
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealType.map((meal, index) => (
+            <option key={index} value={meal.toLowerCase()}>
+              {meal}
+            </option>
+          ))}
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
