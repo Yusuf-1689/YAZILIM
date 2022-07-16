1b979da964bc53ac11a0380b5d2cd5dbcdd015fa
yusuf <myusufsb252015@gmail.com>
2022-07-16 13:45:01 +0300

    reducer

--- React/class-note/rc16_context-reducer/reducer-example/src/App.js
+++ React/class-note/rc16_context-reducer/reducer-example/src/App.js
@@ -1,9 +1,11 @@
-import UseStateExample from './UseStateExample';
+import UseReducerExample from './UseReducerExample';
+
 
 function App() {
   return (
     <div>
-      <UseStateExample />
+      {/* <UseStateExample /> */}
+      <UseReducerExample />
     </div>
   );
 }
---
+++ React/class-note/rc16_context-reducer/reducer-example/src/reducer.jsx
@@ -0,0 +1,9 @@
+
+
+
+export const initialState ={
+    loading:false,
+    dog: '',
+    error: '',
+
+};
