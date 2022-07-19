65b367baeb5910699a24c5927744002f4d823cb8
yusuf <myusufsb252015@gmail.com>
2022-07-19 20:50:24 +0300

    navbar

--- React/class-note/rc18-19_movie-app-solution/src/components/Navbar.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/components/Navbar.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  // const currentUser = { displayName: 'felix franko' };
+  const currentUser = false;
   return (
     <div>
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center ">
+            {currentUser ? (
+              <>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser.displayName}
+                </h5>
+                <button className="ms-2 btn btn-outline-light">Logout</button>
+              </>
+            ) : (
+              <>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/login')}
+                >
+                  Login
+                </button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/register')}
+                >
+                  Register
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
