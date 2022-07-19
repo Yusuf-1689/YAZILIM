baa2ad3d2168f9073954dc1921f2f6596c6edad1
yusuf <myusufsb252015@gmail.com>
2022-07-19 21:39:45 +0300

    login

--- React/class-note/rc18-19_movie-app-solution/src/pages/Login.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/pages/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <p>Login</p>
+    <div className="d-flex justify-content-center">
+      <div className="form-image d-none d-md-block ">
+        <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3 ">Register</h1>
+        <form id="register" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="link">Forgot Password?</div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Register"
+          />
+        </form>
+      </div>
     </div>
   );
 };
