84287a2d6f19cbb09e677977d417f871c2ca6a0c
yusuf <myusufsb252015@gmail.com>
2022-07-20 20:39:01 +0300

    append

--- React/class-note/rc18-19_movie-app-solution/src/auth/firebase.js
+++ React/class-note/rc18-19_movie-app-solution/src/auth/firebase.js
@@ -1,5 +1,11 @@
-import { initializeApp } from "firebase/app";
-import {signInWithEmailAndPassword, createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
 
 // TODO: Replace the following with your app's Firebase project configuration
 //* https://firebase.google.com/docs/auth/web/start
@@ -12,44 +18,60 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-
-export const createUser = async(email, password) => {
+export const createUser = async (email, password, navigate) => {
+  //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
   try {
-   let userCredential = await createUserWithEmailAndPassword(
-    auth, 
-    email, 
-    password
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
     );
-      // navigate('/');
-      console.log(userCredential);
-  } catch (err)
-  {
+    navigate('/');
+    console.log(userCredential);
+  } catch (err) {
     console.log(err);
   }
-  
-}
+};
 
-export const signIn = async(email, password, navigate) => {
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+//! Email/password ile girişi enable yap
+export const signIn = async (email, password, navigate) => {
+  //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
   try {
     let userCredential = await signInWithEmailAndPassword(
-     auth, 
-     email, 
-     password
-     );
-       navigate('/');
-       console.log(userCredential);
-   } catch (err)
-   {
-     console.log(err);
-   }
-}
+      auth,
+      email,
+      password
+    );
+    navigate('/');
+    // sessionStorage.setItem('user', JSON.stringify(userCredential.user));
+    console.log(userCredential);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const userObserver = (setCurrentUser) => {
+  //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
+
+export const logOut = () => {
+  signOut(auth);
+};
