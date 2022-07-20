58115051887939a07722098b65079f68f5113890
yusuf <myusufsb252015@gmail.com>
2022-07-20 17:40:19 +0300

    append

--- React/class-note/rc18-19_movie-app-solution/src/auth/firebase.js
+++ React/class-note/rc18-19_movie-app-solution/src/auth/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
 const firebaseConfig = {
-  apiKey: "AIzaSyADJjUK-UwLaXwQ3cPi353p8gbo0VXdEMs",
-  authDomain: "movie-app-62654.firebaseapp.com",
-  projectId: "movie-app-62654",
-  storageBucket: "movie-app-62654.appspot.com",
-  messagingSenderId: "186294488457",
-  appId: "1:186294488457:web:ae4e3823c19d1945e7e558",
-  measurementId: "G-XP3C7X6MGR"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  
 };
 
 // Initialize Firebase
@@ -21,3 +21,20 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+
+export const createUser = async(email, password) => {
+  try {
+   let userCredential = await createUserWithEmailAndPassword(
+    auth, 
+    email, 
+    password
+    );
+      console.log(userCredential);
+  } catch (err)
+  {
+    console.log(err);
+  }
+  
+}
+
