e217fac9fdef974a72c95b9cde752a3d1463ef3c
yusuf <myusufsb252015@gmail.com>
2022-07-20 19:59:55 +0300

    append

--- React/class-note/rc18-19_movie-app-solution/src/context/AuthContext.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/context/AuthContext.jsx
@@ -1,18 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
 
-
-
-import React from 'react'
-import { useState } from 'react'
-import { createContext } from 'react'
-
-
-const AuthContext = createContext()
+export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(false)
-  return (
-    <AuthContext.Provider value={{ currentUser }}>{}children</AuthContext.Provider>
-  )
-}
+  const [currentUser, setCurrentUser] = useState(true);
 
-export default AuthContext;
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
