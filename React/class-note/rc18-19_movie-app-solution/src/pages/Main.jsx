9980c6fee4939eac2f880d9af6b59be79e6555d5
yusuf <myusufsb252015@gmail.com>
2022-07-20 21:46:02 +0300

    v4

--- React/class-note/rc18-19_movie-app-solution/src/pages/Main.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/pages/Main.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    setLoading(true);
+    axios
+      .get(API)
+      .then((res) => console.log(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
   return (
-    <div>
-      <p>Main</p>
+    <div className="d-flex justify-content-center flex-wrap">
+      {loading ? (
+        <div className="spinner-border text-primary m-5" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        movies?.map((movie) => null)
+      )}
     </div>
   );
 };
