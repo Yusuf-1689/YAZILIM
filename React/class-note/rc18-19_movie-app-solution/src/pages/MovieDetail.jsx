92002f6febb9889c47581d18154fc8fcdb349fbf
yusuf <myusufsb252015@gmail.com>
2022-07-20 23:15:58 +0300

    m1

--- React/class-note/rc18-19_movie-app-solution/src/pages/MovieDetail.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/pages/MovieDetail.jsx
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
   const [movieDetails, setMovieDetails] = useState({});
   const { id } = useParams();
+
+  const {
+    title,
+    poster_path,
+    overview,
+    vote_average,
+    release_date,
+    vote_count,
+  } = movieDetails;
+
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const baseImageUrl = 'https://image.tmdb.org/t/p/w1280';
@@ -19,8 +29,37 @@ const MovieDetail = () => {
   }, [movieDetailBaseUrl]);
 
   return (
-    <div>
-      <p>MovieDetail</p>
+    <div className="container py-5">
+      <h1 className="text-center">{title}</h1>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={poster_path ? baseImageUrl + poster_path : defaultImage}
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title">Overview</h5>
+              <p className="card-text">{overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                {'Release Date : ' + release_date}
+              </li>
+              <li className="list-group-item">{'Rate : ' + vote_average}</li>
+              <li className="list-group-item">
+                {'Total Vote : ' + vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1}>Go Back</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- React/class-note/rc18-19_movie-app-solution/src/router/AppRouter.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/router/AppRouter.jsx
@@ -2,14 +2,22 @@
 
 
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { AuthContext } from '../context/AuthContext';
 import Login from '../pages/Login';
 import Main from '../pages/Main';
 import MovieDetail from '../pages/MovieDetail';
 import Register from '../pages/Register';
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  function PrivateRouter(){
+    return currentUser ? <Outlet /> : <Navigate to='/login' replace />
+
+  }
+
   return (
     <BrowserRouter>
     <Navbar />
@@ -17,7 +25,9 @@ const AppRouter = () => {
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/details/:id' element={<MovieDetail />} />
+        <Route path='/details/:id' element={<PrivateRouter />} />
+          <Route path='/details/:id' element={<MovieDetail />} />
+        <Route/>
       </Routes>
     </BrowserRouter>
   );
