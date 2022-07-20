ea6d6cf9cf225c7e3ffa8a92afaa17629ece9da8
yusuf <myusufsb252015@gmail.com>
2022-07-20 22:22:10 +0300

    main

--- React/class-note/rc18-19_movie-app-solution/src/pages/Main.jsx
+++ React/class-note/rc18-19_movie-app-solution/src/pages/Main.jsx
@@ -10,6 +10,7 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 const Main = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     getMovies(FEATURED_API);
@@ -23,16 +24,32 @@ const Main = () => {
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies(SEARCH_API + searchTerm);
+  };
   return (
-    <div className="d-flex justify-content-center flex-wrap">
-      {loading ? (
-        <div className="spinner-border text-primary m-5" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-      ) : (
-        movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
-      )}
-    </div>
+    <>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="d-flex justify-content-center flex-wrap">
+        {loading ? (
+          <div className="spinner-border text-primary m-5" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
+    </>
   );
 };
 
