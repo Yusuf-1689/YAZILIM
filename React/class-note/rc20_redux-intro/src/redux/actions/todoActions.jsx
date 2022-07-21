524c2510f3dd14ffbff931c28448caa65bb97e98
yusuf <myusufsb252015@gmail.com>
2022-07-21 23:06:26 +0300

    v7

--- React/class-note/rc20_redux-intro/src/redux/actions/todoActions.jsx
+++ React/class-note/rc20_redux-intro/src/redux/actions/todoActions.jsx
@@ -1,5 +1,15 @@
+import { ADD_TODO, CLEAR_TODO, DELETE_TODO, TOGGLE_TODO } from "../types/todoTypes";
 
 
 export const addTodo = (payload) => {
-    return {}
+    return { type: ADD_TODO, payload };
+};
+export const deleteTodo = (payload) => {
+    return { type: DELETE_TODO, payload };
+};
+export const toggleTodo = (payload) => {
+    return { type: TOGGLE_TODO, payload };
+};
+export const clearTodo = (payload) => {
+    return { type: CLEAR_TODO, payload };
 };
