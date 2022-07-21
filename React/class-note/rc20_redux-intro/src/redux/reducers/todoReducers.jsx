f781dd05b8ffa6648913564ab7a7d265ff907b13
yusuf <myusufsb252015@gmail.com>
2022-07-21 23:14:56 +0300

    v7

--- React/class-note/rc20_redux-intro/src/redux/reducers/todoReducers.jsx
+++ React/class-note/rc20_redux-intro/src/redux/reducers/todoReducers.jsx
@@ -1,20 +1,31 @@
-import { ADD_TODO, CLEAR_TODO, DELETE_TODO, TOGGLE_TODO } from "../types/todoTypes";
+import {
+  ADD_TODO,
+  CLEAR_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO,
+} from '../types/todoTypes';
 
 const initialState = {
-  todoList: [{id:new Date().getTime, text: 'Work on Redux', completed: true}]
+  todoList: [
+    { id: new Date().getTime(), text: 'Work On Redux', completed: true },
+  ],
 };
 
 const todoReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ADD_TODO:
-      return { ...state, ...payload };
+      return {
+        todoList: [
+          ...state.todoList,
+          { id: new Date().getTime(), text: payload, completed: false },
+        ],
+      };
     case DELETE_TODO:
-      return { ...state, ...payload };
-    case CLEAR_TODO:
       return { ...state, ...payload };
     case TOGGLE_TODO:
       return { ...state, ...payload };
-
+    case CLEAR_TODO:
+      return { ...state, ...payload };
     default:
       return state;
   }
