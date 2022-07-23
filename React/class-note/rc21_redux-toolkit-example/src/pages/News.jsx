52a8ec3e89f1bfe7cec3f70ea62a3f48c2690b42
yusuf <myusufsb252015@gmail.com>
2022-07-23 13:53:09 +0300

    v7

--- React/class-note/rc21_redux-toolkit-example/src/pages/News.jsx
+++ React/class-note/rc21_redux-toolkit-example/src/pages/News.jsx
@@ -6,43 +6,58 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { CardMedia } from '@mui/material';
 import loadingGif from '../assets/loading.gif';
+import { useDispatch, useSelector } from 'react-redux';
 
 const News = () => {
+  const dispatch = useDispatch();
+  const { newsList, loading } = useSelector((state) => state.news);
+
   return (
     <>
-      <h1>NEWS</h1>
-      <Box
-        xs={{ d: 'flex' }}
-        display="flex"
-        alignItems="center"
-        justifyContent="space-evenly"
-        flexWrap="wrap"
-      >
-        {[].map((item, index) => (
-          <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
-            <CardMedia
-              component="img"
-              height="250"
-              image={item?.urlToImage}
-              alt="img"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {item?.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {item?.content}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small" href={item?.url} target="_blank">
-                Detail
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
-      </Box>
+      {loading && (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="100vh"
+        >
+          <img src={loadingGif} alt="gif" width="90%" height="800px" />
+        </Box>
+      )}
+      {!loading && (
+        <Box
+          xs={{ d: 'flex' }}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-evenly"
+          flexWrap="wrap"
+        >
+          {newsList?.map((item, index) => (
+            <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
+              <CardMedia
+                component="img"
+                height="250"
+                image={item?.urlToImage}
+                alt="img"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item?.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item?.content}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small" href={item?.url} target="_blank">
+                  Detail
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+        </Box>
+      )}
     </>
   );
 };
