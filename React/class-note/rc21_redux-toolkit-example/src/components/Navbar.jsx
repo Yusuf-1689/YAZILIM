1c1e4c8d26a3f3a441a7da2fb830e7a82938e5a2
yusuf <myusufsb252015@gmail.com>
2022-07-23 11:32:48 +0300

    v2

--- React/class-note/rc21_redux-toolkit-example/src/components/Navbar.jsx
+++ React/class-note/rc21_redux-toolkit-example/src/components/Navbar.jsx
@@ -1,20 +1,36 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+const user = true;
+  const navigate = useNavigate();
+  
+const handleLogout = ()=>{
+  navigate("login")
+  
+}  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, cursor: "pointer" }}
+            onClick={() => navigate("/")}
+          >
             Clarusway News
           </Typography>
-
-          <Button color="inherit">Login</Button>
+          {user ? (
+            <Button color="inherit" onClick={handleLogout}>Logout</Button>
+          ) : (
+            <Button color="inherit" onClick={()=>navigate("/")}>Login</Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
