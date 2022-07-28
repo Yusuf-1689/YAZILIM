378cc492a6b4829db22e7f30aedf739d0ab4fecb
yusuf <myusufsb252015@gmail.com>
2022-07-28 20:25:41 +0300

    form1

--- React/class-note/rc24_firecontact-solution/src/App.js
+++ React/class-note/rc24_firecontact-solution/src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Contacts from './components/contacts/Contacts';
 import FormComponent from './components/form/FormComponent';
 
+const initialValues ={username:"", phoneNumber:"", gender:""}
+
 function App() {
+
+const [info, setInfo] = useState(initialValues)
+
+
   return (
     <div className="App">
-      <FormComponent/>
+      <FormComponent info={info} setInfo={setInfo}/>
       <Contacts/>
     </div>
   );
--- React/class-note/rc24_firecontact-solution/src/components/form/FormComponent.js
+++ React/class-note/rc24_firecontact-solution/src/components/form/FormComponent.js
@@ -14,7 +14,14 @@ import {
 import { AccountCircle } from "@mui/icons-material";
 import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 
-const FormComponent = () => {
+const FormComponent = ({info, setInfo}) => {
+
+  const handleChange=(e) => {
+    e.preventDefault();
+    // const name= e.target.name;
+    // const value=e.target.value;
+    const {name,value}= e.target;
+  }
   return (
     <Grid
       textAlign="center"
@@ -43,8 +50,8 @@ const FormComponent = () => {
             <TextField
               variant="outlined"
               name="username"
-              value={null}
-              onChange={null}
+              value={info.phoneNumber}
+              onChange={handleChange}
               placeholder="Name"
               InputProps={{
                 startAdornment: (
