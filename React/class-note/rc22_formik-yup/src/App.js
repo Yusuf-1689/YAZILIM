35db8eb591a68467654ca5fceb48d997639ffe4e
yusuf <myusufsb252015@gmail.com>
2022-07-29 19:19:39 +0300

    v1

--- React/class-note/rc22_formik-yup/src/App.js
+++ React/class-note/rc22_formik-yup/src/App.js
@@ -1,9 +1,21 @@
+import { deepPurple, indigo, green } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import LoginPage from './pages/LoginPage';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <ThemeProvider theme={theme}>
+        <LoginPage />
+      </ThemeProvider>
     </div>
   );
 }
--- React/class-note/rc22_formik-yup/src/pages/LoginPage.jsx
+++ React/class-note/rc22_formik-yup/src/pages/LoginPage.jsx
@@ -1,5 +1,58 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import { FaLock } from 'react-icons/fa';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <Container maxWidth="sm" sx={{ mt: '10rem', textAlign: 'center' }}>
+        <Avatar
+          sx={{
+            backgroundColor: 'primary.main',
+            m: 'auto',
+            width: 60,
+            height: 60,
+          }}
+          sizes="100px"
+        >
+          <FaLock size="40" />
+        </Avatar>
+        <Typography variant="h4" align="center" mb={4} color="primary.light">
+          Login
+        </Typography>
+
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+          <TextField
+            label="Full Name"
+            name="fullName"
+            id="fullName"
+            type="type"
+            variant="outlined"
+          />
+          <TextField
+            label="Email"
+            name="email"
+            id="email"
+            type="email"
+            variant="outlined"
+          />
+          <TextField
+            label="password"
+            name="password"
+            id="password"
+            variant="outlined"
+          />
+          <Button type="submit" variant="contained" size="large">
+            Submit
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default LoginPage;
