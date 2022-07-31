f5cb974b8a1b5b5d2b6795548574fff1049384b8
yusuf <myusufsb252015@gmail.com>
2022-07-31 23:09:55 +0300

    v1

---
+++ Java Script/DERS NOTLARI/JS 30-05 DERSİ/kredi hesaplama/app.js
@@ -0,0 +1,85 @@
+console.log('**** Kredi Hesaplama *******');
+const select = document.querySelector('.form-select');
+const vade = document.querySelector('#vade');
+const tutar = document.querySelector('#tutar');
+const hesaplaBtn = document.querySelector('.btn-dark');
+let oran = 0;
+let taksit = 0;
+
+hesaplaBtn.addEventListener('click', (e) => {
+  //? preventDefault() event'ın default davranaışı (submit etmek ve formu silmek) engeller
+  e.preventDefault();
+  if (select.value === 'Konut Kredisi') {
+    oran = 1.29;
+  } else if (select.value === 'Ihtiyac Kredisi') {
+    oran = 1.99;
+  } else if (select.value === 'Arac Kredisi') {
+    oran = 1.79;
+  }
+  if (!select.value || !vade.value || !tutar.value) {
+    alert('Lutfen Kredi turu, Vade ve tutari giriniz');
+  }
+
+  const faiz = oran / 100;
+  taksit =
+    (tutar.value * (faiz * (1 + faiz) ** vade.value)) /
+    ((1 + faiz) ** vade.value - 1);
+
+  // console.log(taksit);
+  sonuclariGoster();
+});
+const sonuclariGoster = () => {
+  const sonuclar = document.querySelector('.sonuclar');
+
+  sonuclar.innerHTML = `
+  <h2 class="mt-3 text-warning">Kredi Bilgileri</h2>
+  <table class="table table-bordered border-warning  mt-4">
+   <tbody>
+    <tr>
+      <th>Kredi Miktari</th>
+      <td>${tutar.value} ₺</td>
+      <th>Kredi Tipi</th>
+      <td>${select.value}</td>
+    </tr>
+    <tr>
+      <th>Vade</th>
+      <td>${vade.value}</td>
+      <th>Faiz Orani</th>
+      <td>${oran}</td>
+    </tr>
+    <tr>
+      <th>Toplam Tutar</th>
+      <td>${(taksit * vade.value).toFixed(2)} ₺</td>
+      <th>Taksit Tutari</th>
+      <td>${taksit.toFixed(2)} ₺</td>
+    </tr>
+  </tbody>
+</table>
+  `;
+};
+//!-------------------------TW-5 Çalışması-------------------------------
+//!----------------------------------------------------------
+// const arr1 = ['a', 'b', 'c'];
+// const arr2 = ['b', 'c', 'a'];
+// console.log(
+//  arr1.sort() === arr1,
+//  arr2.sort() == arr2,
+//  arr1.sort() === arr2.sort()
+// );
+
+// const notifications = 1;
+// console.log(
+//  `You have ${notifications} notification${notifications !== 1 && 's'}`
+// );
+
+// document.querySelectorAll('div').forEach((e) => {
+//   e.onclick = (e) => console.log(e.currentTarget.id);
+//  });
+
+
+
+
+
+
+
+
