3676775e875da54716aae2b8f48d95ccd61dcd71
yusuf <myusufsb252015@gmail.com>
2022-08-03 21:52:28 +0300

    correction

--- React/project/language-card-project/src/components/card/Card.js
+++ React/project/language-card-project/src/components/card/Card.js
@@ -8,9 +8,13 @@ const Card = () => {
       <div className="bars"></div>
       <h1 className="language-title">Languages</h1>
       <div className="cards-container">
-        {languages.map((item, index) => {
-          return <Item card={item} />;
-        })}
+        {
+        languages.map((item, index) => {
+          return  <Item card={item} />;
+          
+          })
+        
+        }
       </div>
     </div>
   );
