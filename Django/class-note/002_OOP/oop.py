f0e23a23f9fbc33ab51e487496189e9c641ab0db
yusuf <myusufsb252015@gmail.com>
2022-08-16 20:34:25 +0300

    append

--- Django/class-note/002_OOP/oop.py
+++ Django/class-note/002_OOP/oop.py
@@ -1,2 +1,22 @@
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
+
+os.system("cls" if os.name == "nt" else "clear")
+
+print("-------------------------------------------------")
+
+
+class Person:
+    name = "Yusuf"
+    age = 30
+
+
+# ? Person class'ından obje (instance) üretiyoruz 👇
+person1 = Person()
+person2 = Person()
+
+print(person1.name)  # Victor
+
+# ? Class'ımıza yeni bir attribute ekledik. Bu class'tan üretilmiş instance'larda bu özellik geçerli oluyor.
+Person.job = "Developer"
+
+print(person2.job)
