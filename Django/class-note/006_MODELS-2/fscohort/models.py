3e7c023a22d876ea38b6db71847e55df2f8b6090
yusuf <myusufsb252015@gmail.com>
2022-08-22 22:12:54 +0300

    append

--- Django/class-note/006_MODELS-2/fscohort/models.py
+++ Django/class-note/006_MODELS-2/fscohort/models.py
@@ -1,12 +1,25 @@
 
 
 
+from distutils.command.upload import upload
+from turtle import update
 from django.db import models
 
 class Student(models.Model):
+    COUNTRIES = [
+        ('TR', 'Turkey'),
+        ('US', 'America'),
+        ('DE', 'Germany'),
+        ('FR', 'France'),
+    ]
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField('Numara')
+    about = models.TextField('Hakkında', blank=True, null=True)
+    country = models.CharField('Ülke', max_length=2 choices=COUNTRIES, default='TR')
+    avatar = models.ImageField('Resim', blank=True, null=True, upload_to='media/')
+    registered_date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
--- Django/class-note/006_MODELS-2/main/urls.py
+++ Django/class-note/006_MODELS-2/main/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('', home),
     path('fscohort/', include('fscohort.urls')), # -> fscohort/urls.py
 ]
+
+# View Static/Media Files:
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
