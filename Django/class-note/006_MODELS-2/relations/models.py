c53866a972db315733c413ec4d57360bb7c9f56f
yusuf <myusufsb252015@gmail.com>
2022-08-22 22:43:49 +0300

    append

--- Django/class-note/006_MODELS-2/relations/models.py
+++ Django/class-note/006_MODELS-2/relations/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+
+from django.db import models
+class Creator(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+# one2one relation:
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    creator = models.models.OneToOneField(Creator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
