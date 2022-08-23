98a9cd27775b59d6f2c99fe0550f6d27124494d2
yusuf <myusufsb252015@gmail.com>
2022-08-23 12:24:02 +0300

    update

--- Django/class-note/006_MODELS-2/main/urls.py
+++ Django/class-note/006_MODELS-2/main/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('fscohort/', include('fscohort.urls')), # -> fscohort/urls.py
 ]
 
-# View Static/Media Files:
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # View Static/Media Files:
+# from django.conf import settings
+# from django.conf.urls.static import static
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
