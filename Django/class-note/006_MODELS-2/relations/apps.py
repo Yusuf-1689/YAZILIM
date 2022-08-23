49dd89bc99cc511f2100e107138e7ef46f166976
yusuf <myusufsb252015@gmail.com>
2022-08-23 15:47:01 +0300

    update

--- Django/class-note/006_MODELS-2/relations/apps.py
+++ Django/class-note/006_MODELS-2/relations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RelationsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "relations"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relations'
