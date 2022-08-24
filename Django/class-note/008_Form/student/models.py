862ee1342eff0112237cff8546c3552244162a10
yusuf <myusufsb252015@gmail.com>
2022-08-24 21:36:55 +0300

    append

--- Django/class-note/008_Form/student/models.py
+++ Django/class-note/008_Form/student/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    number = models.IntegerField(blank=True, null=True)
+    number = models.IntegerField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
