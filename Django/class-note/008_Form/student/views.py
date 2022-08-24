5d5661b538c0b984d23290f1ee953f23de28e4fe
yusuf <myusufsb252015@gmail.com>
2022-08-24 22:29:56 +0300

    append

--- Django/class-note/008_Form/student/views.py
+++ Django/class-note/008_Form/student/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
 
+from student.models import Student
+from .forms import StudentForm
+
+
 def index(request):
     return render(request, 'student/index.html')
 
+
+# def student_page(request):
+#     return render(request, 'student/student.html')
+
+
+# def student_page(request):
+    # print(request.POST)
+    # print(request.FILES)
+    # form = StudentForm()
+    # context = {
+    #     'form': form
+    # }
+
+    # return render(request, 'student/student.html', context)
+
+
 def student_page(request):
-    return render(request,'student/student.html')
+    form = StudentForm()
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            student_data = {
+                "first_name": form.cleaned_data.get('first_name'),
+                "last_name": form.cleaned_data.get("last_name"),
+                "number": form.cleaned_data.get("number"),
+                "profile_pic": form.cleaned_data.get("profile_image")
+            }
+            # Student.objects.create(**student_data)
+            student = Student(**student_data)
+            student.save()
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'student/student.html', context)
