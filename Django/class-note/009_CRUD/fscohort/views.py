e785155325ebb5bb4e290a178338b1b2d18acc22
yusuf <myusufsb252015@gmail.com>
2022-08-25 21:25:59 +0300

    append

--- Django/class-note/009_CRUD/fscohort/views.py
+++ Django/class-note/009_CRUD/fscohort/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
 def index(request):
     return render (request, 'fscohort/index.html')
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students,
+    }
+    return render(request, 'fscohort/student_list.html', context)
