57d78d52735fca4c23407e6dc206256042f63cec
yusuf <myusufsb252015@gmail.com>
2022-08-26 23:03:01 +0300

    update

--- Django/class-note/009_CRUD/fscohort/views.py
+++ Django/class-note/009_CRUD/fscohort/views.py
@@ -53,9 +53,9 @@ def student_delete(request,id):
     }
     return render(request,'fscohort/student_delete.html',context)
 
-# def student_detail(request, id):        
-#     student = Student.objects.get(id=id)
-#     context = {
-#         'student': student
-#     }
-#     return render(request, 'fscohort/student_detail.html', context)
+def student_detail(request, id):        
+    student = Student.objects.get(id=id)
+    context = {
+        'student': student
+    }
+    return render(request, 'fscohort/student_detail.html', context)
