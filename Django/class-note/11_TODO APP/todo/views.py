10f305e6e84f7c0b8f64ee54a86acf7f70bc2c07
yusuf <myusufsb252015@gmail.com>
2022-08-29 20:23:39 +0300

    urls

--- Django/class-note/11_TODO APP/todo/views.py
+++ Django/class-note/11_TODO APP/todo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Welcome")
