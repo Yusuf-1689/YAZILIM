1f3333f4a8684f4d2ec772a68295aa2b3d69f61f
yusuf <myusufsb252015@gmail.com>
2022-08-29 21:13:16 +0300

    append

--- Django/class-note/11_TODO APP/todo/views.py
+++ Django/class-note/11_TODO APP/todo/views.py
@@ -1,14 +1,20 @@
-
-
-from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
-
+from .forms import TodoForm
 
 def home(request):
     todos = Todo.objects.all()
-    context={
-        "todos": todos
+    context = {
+        "todos" : todos
     }
     return render(request, "todo/home.html", context)
+
+
+def todo_create(request):
+    form = TodoForm()
+    
+    context = {
+        "form" : form
+    }
+    return render(request, "todo/todo_add.html", context)
