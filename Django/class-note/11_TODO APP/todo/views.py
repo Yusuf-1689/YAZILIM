76a417ca2e0451e367ea24dfd83522bc3279f60d
yusuf <myusufsb252015@gmail.com>
2022-08-29 20:49:35 +0300

    home

--- Django/class-note/11_TODO APP/todo/views.py
+++ Django/class-note/11_TODO APP/todo/views.py
@@ -1,9 +1,14 @@
 
 
-import re
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 
 def home(request):
-    return HttpResponse("Welcome")
+    todos = Todo.objects.all()
+    context={
+        "todos": todos
+    }
+    return render(request, "todo/home.html", context)
