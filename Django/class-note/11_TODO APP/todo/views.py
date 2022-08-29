df1bc031cf8be681e6989805e9f8d27050f121c0
yusuf <myusufsb252015@gmail.com>
2022-08-29 22:16:09 +0300

    append

--- Django/class-note/11_TODO APP/todo/views.py
+++ Django/class-note/11_TODO APP/todo/views.py
@@ -6,27 +6,34 @@ from .forms import TodoForm
 def home(request):
     todos = Todo.objects.all()
     form = TodoForm()
+    
     context = {
         "todos" : todos,
         "form" : form
     }
-
     return render(request, "todo/home.html", context)
+
 
 def todo_create(request):
     form = TodoForm()
-
+    
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
-
+    
     context = {
         "form" : form
     }
-
     return render(request, "todo/todo_add.html", context)
 
-
-
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+    
+    context = {
+        "todo" : todo,
+        "form" : form
+    }
+    return render(request, "todo/todo_update.html", context)
