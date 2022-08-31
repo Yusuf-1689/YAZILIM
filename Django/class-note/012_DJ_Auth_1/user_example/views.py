a15fb2ee1e02f1667a2292a55c0e8dbcd5af156d
yusuf <myusufsb252015@gmail.com>
2022-08-31 21:46:11 +0300

    register html

--- Django/class-note/012_DJ_Auth_1/user_example/views.py
+++ Django/class-note/012_DJ_Auth_1/user_example/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -10,7 +11,21 @@ def special(request):
 
 
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm(request.POST or None)
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+        username = form.cleaned_data('username')
+        password = form.cleaned_data('password1')
+        
+        user = authenticate(username=username, password=password)
+        
+        login(request, user)
+        
+        return redirect('home')
     
     context = {
         'form':form
