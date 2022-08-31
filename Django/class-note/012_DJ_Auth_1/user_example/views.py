52aced934bf6dc05fa27baffdb7e1587cde55957
yusuf <myusufsb252015@gmail.com>
2022-08-31 22:49:01 +0300

    update

--- Django/class-note/012_DJ_Auth_1/user_example/views.py
+++ Django/class-note/012_DJ_Auth_1/user_example/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'user_example/index.html')
 
-
+@login_required
 def special(request):
     return render(request, "user_example/special.html")
 
