52550714425a15e3e367c91f26cd361d65e144e1
yusuf <myusufsb252015@gmail.com>
2022-09-01 20:37:43 +0300

    append

--- Django/class-note/013_DJ_Auth_2/main/settings.py
+++ Django/class-note/013_DJ_Auth_2/main/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
