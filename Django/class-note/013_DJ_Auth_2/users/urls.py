b1f91c32e8f8f27dd018d3d07d267a8b2484a6ee
yusuf <myusufsb252015@gmail.com>
2022-09-05 12:51:41 +0300

    append

--- Django/class-note/013_DJ_Auth_2/users/urls.py
+++ Django/class-note/013_DJ_Auth_2/users/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import user_logout,register
+from .views import user_login, user_logout,register
 
 urlpatterns = [
     path('logout/', user_logout,name='user_logout'),
     path('register/', register,name='register'),
+    path('login/', user_login,name='login'),
 ]
--- Django/class-note/013_DJ_Auth_2/users/views.py
+++ Django/class-note/013_DJ_Auth_2/users/views.py
@@ -1,6 +1,8 @@
+import logging
 from multiprocessing import context
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login
+from django.contrib.auth.forms import AuthenticationForm
 
 from django.contrib import messages
 
@@ -19,9 +21,33 @@ def user_logout(request):
 def register(request):
     form_user=UserForm()
     form_profile=UserProfileForm()
+    if request.method == 'POST':
+        form_user=UserForm(request.POST)
+        form_profile=UserProfileForm(request.POST, request.FILES)
+        if form_user.isvalid() and form_profile.is_valid():
+            user=form_user.save()
+            # form_profile.save()
+            profile=form_profile.save(commit=False)
+            profile.user=user
+            profile.save()
+            login(request,user)
+            
+            
+            return redirect('home')
+    
+    
     context[
         'form_profile':form_profile,
         'form_user':form_user
     ]
     return render(request, 'users/register.html',context)
     
+    
+def user_login(request):
+    form=AuthenticationForm(request,data=request.POST)
+    if form.is_valid():
+        user=form.get_user()
+        login(request,user)
+        return redirect('home')
+    
+    return render(request, 'users/login.html',{'forn':form})
