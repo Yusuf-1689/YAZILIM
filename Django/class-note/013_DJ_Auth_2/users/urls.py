73cb0a6383005c83c34231cf571a34bb692bc3ec
yusuf <myusufsb252015@gmail.com>
2022-09-05 12:11:41 +0300

    append

--- Django/class-note/013_DJ_Auth_2/users/urls.py
+++ Django/class-note/013_DJ_Auth_2/users/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import user_logout
+from .views import user_logout,register
 
 urlpatterns = [
     path('logout/', user_logout,name='user_logout'),
+    path('register/', register,name='register'),
 ]
