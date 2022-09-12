1e36ae2911755256241b23f273eab723a20dfd76
yusuf <myusufsb252015@gmail.com>
2022-09-12 22:26:22 +0300

    append

--- Django/class-note/014_Django_Test/hashing/tests.py
+++ Django/class-note/014_Django_Test/hashing/tests.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from django.test import TestCase
 
+from .forms import HashForm
+
+import hashlib
+
 # browser = webdriver.Chrome()
 # browser.get('http://localhost:8000')
 
@@ -32,3 +36,19 @@ class UnitTestCase(TestCase):
         response = self.client.get('/')
         # Search for home.html
         self.assertTemplateUsed(response, 'hashing/home.html')
+        
+    def test_hash_form(self):
+        # Check if there is a form, needs to filled with some data, for not to be an empty form which basically equals to False
+        form = HashForm(data={'text': 'hello'})
+        # Check if it is valid
+        self.assertTrue(form.is_valid())
+        
+    def test_hash_func_works(self):
+        # Need to import haslib library to generate hash
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        # hash.hexdigest() Like digest() except the digest is returned as a string object of double length, containing only hexadecimal digits. This may be used to exchange the value safely in email or other non-binary environments.
+        # Info about hashlib: https://docs.python.org/3/library/hashlib.html
+        # Check our result equals to the true one
+        # Go to any of the website to generate hash
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
+        # No need to write extra code for that, because it checks the library of hashlib only.
