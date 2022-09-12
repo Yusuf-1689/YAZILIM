85a023c5711a5ceb4441dc213fb1d21187ed0f5d
yusuf <myusufsb252015@gmail.com>
2022-09-12 21:29:49 +0300

    apdate

--- Django/class-note/014_Django_Test/hashing/tests.py
+++ Django/class-note/014_Django_Test/hashing/tests.py
@@ -1,4 +1,16 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+# browser = webdriver.Chrome()
+# browser.get('http://localhost:8000')
+
+class FunctionalTestCase(TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter Hash Here',self.browser.page_source)
