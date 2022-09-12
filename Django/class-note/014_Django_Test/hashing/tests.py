84d33d2fab508d6b470bf76cd4436dbca80b23e5
yusuf <myusufsb252015@gmail.com>
2022-09-12 21:50:23 +0300

    update

--- Django/class-note/014_Django_Test/hashing/tests.py
+++ Django/class-note/014_Django_Test/hashing/tests.py
@@ -17,14 +17,9 @@ class FunctionalTestCase(TestCase):
         self.assertIn('Enter hash here',self.browser.page_source)
 
     
-     def test_hash_of_hello(self):
+    def test_hash_of_hello(self):
         self.browser.get('http://localhost:8000')
-        # Find the element with id "text"
         text = self.browser.find_element_by_id("id_text")
-        # Simulate user types "hello"
-        # send_keys method is used to send text to any field, such as input field of a form or even to anchor tag paragraph, etc. It replaces its contents on the webpage in your browser.
         text.send_keys("hello")
-        # Simulate click to the submit button
         self.browser.find_element_by_name("submit").click()
-        # Result must be the correct walue
-        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+        self.assertInHTML("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", self.browser.page_source)
