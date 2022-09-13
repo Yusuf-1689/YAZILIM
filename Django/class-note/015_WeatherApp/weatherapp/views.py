cf53c4304df479f65ea67018cdbd72577cf96fe6
yusuf <myusufsb252015@gmail.com>
2022-09-13 21:31:56 +0300

    update

--- Django/class-note/015_WeatherApp/weatherapp/views.py
+++ Django/class-note/015_WeatherApp/weatherapp/views.py
@@ -1,44 +1,42 @@
-from multiprocessing import context
 from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
 from django.contrib import messages
+from .models import City
 
 def index(request):
     API_KEY = config("API_KEY")
     city = "Mersin"
-    u_city = request.POST.get("name")
+    user_city = request.POST.get("name")
     
-    if u_city:
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    if user_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         print(response.ok)
-        
-        
-    if response.ok:
-        pass
-    else:
-        messages.warning(request, "There is no city")
-    
-    
-        
-    
-    
-    
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+        if response.ok:
+            pass
+        else:
+            messages.warning(request, "❌ There is no city ❌")
+
+
+    url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
+    #? We converted the incoming data to python dictionary format 👇
     content = response.json()
+    print(response) #! 👉 <Response [200]>
+    # pprint(content)
     pprint(content["name"])
     pprint(content["main"]["temp"])
     pprint(content["weather"][0]["description"])
     pprint(content["weather"][0]["icon"])
-    
+
     context = {
         "city": content["name"],
         "temp": content["main"]["temp"],
         "icon": content["weather"][0]["icon"],
         "desc": content["weather"][0]["description"]
     }
-    
+
     return render(request, 'weatherapp/index.html', context)
