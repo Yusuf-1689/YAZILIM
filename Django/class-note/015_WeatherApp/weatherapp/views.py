ee17363471561a16721774868639abcadb8034e3
yusuf <myusufsb252015@gmail.com>
2022-09-13 21:26:48 +0300

    append

--- Django/class-note/015_WeatherApp/weatherapp/views.py
+++ Django/class-note/015_WeatherApp/weatherapp/views.py
@@ -3,14 +3,32 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
 
 def index(request):
     API_KEY = config("API_KEY")
     city = "Mersin"
+    u_city = request.POST.get("name")
+    
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        print(response.ok)
+        
+        
+    if response.ok:
+        pass
+    else:
+        messages.warning(request, "There is no city")
+    
+    
+        
+    
+    
+    
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     content = response.json()
-    # pprint(content)
     pprint(content["name"])
     pprint(content["main"]["temp"])
     pprint(content["weather"][0]["description"])
