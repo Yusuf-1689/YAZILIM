a86c698ec621a8da96fbf5009379c0f4335772d3
yusuf <myusufsb252015@gmail.com>
2022-09-13 20:33:43 +0300

    append

--- Django/class-note/015_WeatherApp/weatherapp/views.py
+++ Django/class-note/015_WeatherApp/weatherapp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from decouple import config
+import requests
 
 def index(request):
+    API_KEY = config("API_KEY")
+    city = "Yozgat"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    print(response)
+    
     return render(request, 'weatherapp/index.html')
