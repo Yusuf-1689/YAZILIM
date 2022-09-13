3cb2c7cea13bf4cdf1e12f5c9cec7ab561aea06f
yusuf <myusufsb252015@gmail.com>
2022-09-13 20:55:23 +0300

    append

--- Django/class-note/015_WeatherApp/weatherapp/views.py
+++ Django/class-note/015_WeatherApp/weatherapp/views.py
@@ -1,13 +1,26 @@
+from multiprocessing import context
 from django.shortcuts import render
 from decouple import config
 import requests
+from pprint import pprint
 
 def index(request):
     API_KEY = config("API_KEY")
-    city = "Mersin"
+    city = "Yozgat"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     response = requests.get(url)
     content = response.json()
-    print(content)
+    # pprint(content)
+    pprint(content["name"])
+    pprint(content["main"]["temp"])
+    pprint(content["weather"][0]["description"])
+    pprint(content["weather"][0]["icon"])
     
-    return render(request, 'weatherapp/index.html')
+    context = {
+        "city": content["name"],
+        "temp": content["main"]["temp"],
+        "icon": content["weather"][0]["icon"],
+        "desc": content["weather"][0]["description"]
+    }
+    
+    return render(request, 'weatherapp/index.html', context)
