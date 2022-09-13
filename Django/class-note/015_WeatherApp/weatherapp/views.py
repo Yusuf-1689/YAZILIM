ca2cec4d55d843e1e9b770d6ba953e54ae5266d0
yusuf <myusufsb252015@gmail.com>
2022-09-13 21:57:28 +0300

    append

--- Django/class-note/015_WeatherApp/weatherapp/views.py
+++ Django/class-note/015_WeatherApp/weatherapp/views.py
@@ -7,36 +7,42 @@ from .models import City
 
 def index(request):
     API_KEY = config("API_KEY")
-    city = "Mersin"
-    user_city = request.POST.get("name")
+    city = "Erzurum"
+    u_city = request.POST.get("name")
     
-    if user_city:
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={API_KEY}&units=metric"
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         print(response.ok)
-
+        
         if response.ok:
-            pass
+            content = response.json()
+            r_city = content["name"]
+            if City.objects.filter(name=r_city):
+                messages.warning(request, "City already exists!")
+            else:
+                City.objects.create(name=r_city)
+            
         else:
-            messages.warning(request, "❌ There is no city ❌")
+            messages.warning(request, "There is no city")
+            
+    city_data = []
+    cities = City.objects.all()
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        content = response.json()
+        data = {
+            "city" : content["name"],
+            "temp" : content["main"]["temp"],
+            "icon" : content["weather"][0]["icon"],
+            "desc" : content["weather"][0]["description"]
+        }
+        city_data.append(data)
 
-
-    url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    #? We converted the incoming data to python dictionary format 👇
-    content = response.json()
-    print(response) #! 👉 <Response [200]>
-    # pprint(content)
-    pprint(content["name"])
-    pprint(content["main"]["temp"])
-    pprint(content["weather"][0]["description"])
-    pprint(content["weather"][0]["icon"])
 
     context = {
-        "city": content["name"],
-        "temp": content["main"]["temp"],
-        "icon": content["weather"][0]["icon"],
-        "desc": content["weather"][0]["description"]
+        "city_data": city_data
     }
-
+    
     return render(request, 'weatherapp/index.html', context)
