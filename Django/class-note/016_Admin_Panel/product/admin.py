20f31e4ac3c1c4b86dd612022a7889b069579936
yusuf <myusufsb252015@gmail.com>
2022-09-14 21:17:14 +0300

    append

--- Django/class-note/016_Admin_Panel/product/admin.py
+++ Django/class-note/016_Admin_Panel/product/admin.py
@@ -1,15 +1,15 @@
-
-
 from django.contrib import admin
 
+# Register your models here.
 from .models import Product
+# readonly_fields = ("create_date",)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "create_date", "is_in_stock", "update_date")
-    list_editable = ( "is_in_stock", )
-    
-    
-admin.site.register(Product, ProductAdmin)
+    list_editable = ( "is_in_stock",)
+
+
+admin.site.register(Product,ProductAdmin)
 
 admin.site.site_title = "Clarusway Title"
 admin.site.site_header = "Clarusway Admin Portal"  
--- Django/class-note/016_Admin_Panel/product/models.py
+++ Django/class-note/016_Admin_Panel/product/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
-	# is_in_stock = models.BooleanField(default=True)
+    is_in_stock = models.BooleanField(default=True)
     slug = models.SlugField(null=True, blank=True)
 
     class Meta:
