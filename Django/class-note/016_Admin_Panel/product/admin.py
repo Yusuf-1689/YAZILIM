bca9588e57c2dd2a0e7a7a44ef77b47a442467b0
yusuf <myusufsb252015@gmail.com>
2022-09-14 21:24:50 +0300

    append

--- Django/class-note/016_Admin_Panel/product/admin.py
+++ Django/class-note/016_Admin_Panel/product/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product
-# readonly_fields = ("create_date",)
 
 class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields = ("create_date",)
     list_display = ("name", "create_date", "is_in_stock", "update_date")
     list_editable = ( "is_in_stock",)
+    list_display_links = ("create_date", "name")
+    ordering = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}
+    list_per_page = 25
 
 
 admin.site.register(Product,ProductAdmin)
