15ab9e49a9a9d2ca8ca969b9c56f4965a854be92
yusuf <myusufsb252015@gmail.com>
2022-09-15 21:31:29 +0300

    template

--- Django/class-note/016_Admin_Panel/main/settings.py
+++ Django/class-note/016_Admin_Panel/main/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
