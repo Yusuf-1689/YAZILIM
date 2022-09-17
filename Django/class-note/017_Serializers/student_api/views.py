0e5aa1e341bc58417d6dfe93aee97a9401e9d4b7
yusuf <myusufsb252015@gmail.com>
2022-09-17 12:53:14 +0300

    append

--- Django/class-note/017_Serializers/student_api/views.py
+++ Django/class-note/017_Serializers/student_api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, HttpResponse, get_object_or_404
 
 from .models import Student
 
-# from .serializers import StudentSerializer
+from .serializers import StudentSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
