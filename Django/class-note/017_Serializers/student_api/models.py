42ec26fcc9bc3efa44935e4e3d9c1a4ae9386afc
yusuf <myusufsb252015@gmail.com>
2022-09-19 16:18:00 +0300

    update

--- Django/class-note/017_Serializers/student_api/models.py
+++ Django/class-note/017_Serializers/student_api/models.py
@@ -6,7 +6,7 @@ class Path(models.Model):
 
     def __str__(self):
         return f"{self.path_name}"
-    
+
 class Student(models.Model):
     path = models.ForeignKey(Path, related_name='students', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
--- Django/class-note/017_Serializers/student_api/serializers.py
+++ Django/class-note/017_Serializers/student_api/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from .models import Student,Path
 
-
-
-#  ----------------------------------1. Yöntem ---------------------------------
+#1. yöntem
 # class StudentSerializer(serializers.Serializer):
 #     first_name = serializers.CharField(max_length=30)
 #     last_name = serializers.CharField(max_length=30)
@@ -18,38 +16,24 @@ from .models import Student,Path
 #         instance.number = validated_data.get('number', instance.number)
 #         instance.save()
 #         return instance
-
-
-# -------------------------------2. Yöntem-------------------------------------------
-
+#2. yöntem
 class PathSerializer(serializers.ModelSerializer):
     class Meta:
         model = Path
         fields = ["id", "path_name"]
-
+    
 class StudentSerializer(serializers.ModelSerializer):
-    full_name=serializers.SerializerMethodField()
-    number_name=serializers.SerializerMethodField()
-    
-    def get_full_name(self,obj):
-        return f'{obj.first_name}{obj.last_name}'
-    
-    def get_number_name(self,obj):
-        return f'{obj.number}{obj.last_name}'
+    # full_name=serializers.SerializerMethodField()
+    # number_name=serializers.SerializerMethodField()
+
+    # def get_full_name(self,obj):
+    #     return f'{obj.first_name}{obj.last_name}'
+    # def get_number_name(self,obj):
+    #     return f'{obj.number}{obj.first_name}'
+    path=serializers.StringRelatedField()
+    path_id=serializers.IntegerField()
     class Meta:
         model = Student
-        fields = ["id","number_name","full_name", "first_name", "last_name", "number"]
+        fields = ["id","path_id","path","first_name", "last_name", "number"]
         # fields = '__all__'
         # exclude = ['number']
-        
-class PathSerializer(serializers.ModelSerializer):
-    students = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
-    class Meta:
-        model = Path
-        fields = "__all__"
-
-class StudentSerializer(serializers.ModelSerializer):
-    path = serializers.StringRelatedField()
-    class Meta:
-        model = Student
-        fields = "__all__"
