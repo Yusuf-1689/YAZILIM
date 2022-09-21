e37cff28c18e8f9ad9348c447600b8bfcd5a9ec0
yusuf <myusufsb252015@gmail.com>
2022-09-21 21:20:23 +0300

    start

---
+++ Django/class-note/020_Pagination_Filters_Search/student_api/pagination.py
@@ -0,0 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
+class SmallPageNumberPagination(PageNumberPagination):
+    page_size = 8
+    page_query_param = "sayfa"
+
+# Local Settings:
+from rest_framework.pagination import LimitOffsetPagination
+class MyLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 3
+    limit_query_param = 'adet'  
