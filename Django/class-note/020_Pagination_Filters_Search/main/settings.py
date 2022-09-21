a2fe28b29d2c06ba7a1da8f57f0ea60862d9e547
yusuf <myusufsb252015@gmail.com>
2022-09-21 21:45:30 +0300

    append

--- Django/class-note/020_Pagination_Filters_Search/main/settings.py
+++ Django/class-note/020_Pagination_Filters_Search/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'student_api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -137,4 +138,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
 #     'PAGE_SIZE': 3
+# }
+
+# PagePagination:
+REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 15
 # }
