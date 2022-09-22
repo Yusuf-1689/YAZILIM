3fd5e6079c66c8ab93f937060ebc42189411a963
yusuf <myusufsb252015@gmail.com>
2022-09-22 22:16:06 +0300

    append

--- Django/class-note/021_DRF_Auth_Permissions/user_api/urls.py
+++ Django/class-note/021_DRF_Auth_Permissions/user_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterView
 
 urlpatterns = [
     path('login/', obtain_auth_token, name="login"),
-
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- Django/class-note/021_DRF_Auth_Permissions/user_api/views.py
+++ Django/class-note/021_DRF_Auth_Permissions/user_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import RegistrationSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
