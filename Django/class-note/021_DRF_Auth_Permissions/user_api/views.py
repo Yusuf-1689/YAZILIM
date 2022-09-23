9e2e9a9bc20cf7c67ddd5955f32902c541458cb6
yusuf <myusufsb252015@gmail.com>
2022-09-23 21:32:35 +0300

    append

--- Django/class-note/021_DRF_Auth_Permissions/user_api/views.py
+++ Django/class-note/021_DRF_Auth_Permissions/user_api/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -14,8 +15,20 @@ class RegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        token = Token.objects.create(user=user)
+        # token = Token.objects.create(user=user)
+        token = Token.objects.get(user=user)
         data = serializer.data
         data['token'] = token.key
+        data["message"] = "user created successfully"
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        data = {
+            'message': 'logout'
+        }
+        return Response(data)
