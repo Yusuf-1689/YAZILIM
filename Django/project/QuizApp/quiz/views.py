d8bc63371d58a784f21feb0df67cdd948b9afaa6
yusuf <myusufsb252015@gmail.com>
2022-09-26 22:46:32 +0300

    append

--- Django/project/QuizApp/quiz/views.py
+++ Django/project/QuizApp/quiz/views.py
@@ -1,4 +1,3 @@
-from re import search
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
@@ -11,24 +10,23 @@ from .models import (
 )
 from .serializers import (
     CategorySerializer,
-    QuizSerializer
+    QuizSerializer,
+    QuestionSerializer
 )
-
 
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
 class QuizList(generics.ListAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
-    filter_backends = [DjangoFilterBackend]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category']
     search_fields = ['title']
-    
 
 class QuestionList(generics.ListAPIView):
     queryset = Question.objects.all()
-    serializers = ''
+    serializer_class = QuestionSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['quiz', 'difficulty']
