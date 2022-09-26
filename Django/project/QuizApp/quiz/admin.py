085049e93b71393322e3a76ef8a3309a9bbc0161
yusuf <myusufsb252015@gmail.com>
2022-09-26 20:55:30 +0300

    append

--- Django/project/QuizApp/quiz/admin.py
+++ Django/project/QuizApp/quiz/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import(
+import nested_admin
+from .models import (
     Category,
     Quiz,
     Question,
     Option
 )
 
+
+class OptionAdmin(nested_admin.NestedTabularInline):
+    model = Option
+    extra = 5
+
+
+class QuestionAdmin(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [OptionAdmin]
+    extra = 5
+    max_num = 20
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionAdmin]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Option)
