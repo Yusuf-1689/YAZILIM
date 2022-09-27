d6229c965424c7dd3e034868281c7e9b716e4368
yusuf <myusufsb252015@gmail.com>
2022-09-27 23:00:12 +0300

    dev.py

--- Django/project/FlightAppsWaggerDebugToolbar/main/settings/dev.py
+++ Django/project/FlightAppsWaggerDebugToolbar/main/settings/dev.py
@@ -1,36 +1,27 @@
 from .base import *
 
 THIRD_PARTY_APPS = ["debug_toolbar"]
-
 DEBUG = config("DEBUG")
-
 INSTALLED_APPS += THIRD_PARTY_APPS
 
 THIRD_PARTY_MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-
 MIDDLEWARE += THIRD_PARTY_MIDDLEWARE
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
- "default": {
- "ENGINE": "django.db.backends.sqlite3",
- "NAME": BASE_DIR / "db.sqlite3",
- }
- 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-
-INTERNAL_IPS = [
- "127.0.0.1",
+INTERNAL_IPS = [ 
+    "127.0.0.1", 
 ]
-
 LOGGING = {
     "version": 1,
     # is set to True then all loggers from the default configuration will be disabled.
     "disable_existing_loggers": True,
-    # Formatters describe the exact format of that text of a log record.
+    # Formatters describe the exact format of that text of a log record. 
     "formatters": {
         "standard": {
             "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s"
@@ -45,13 +36,13 @@ LOGGING = {
         },
     },
     # The handler is the engine that determines what happens to each message in a logger.
-    # It describes a particular logging behavior, such as writing a message to the screen,
+    # It describes a particular logging behavior, such as writing a message to the screen, 
     # to a file, or to a network socket.
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
             "formatter": "standard",
-            "level": "INFO",
+            "level": "ERROR",
             "stream": "ext://sys.stdout",
             },
         'file': {
@@ -65,10 +56,10 @@ LOGGING = {
     "loggers": {
         "django": {
             "handlers": ["console", 'file'],
-            # log level describes the severity of the messages that the logger will handle.
+            # log level describes the severity of the messages that the logger will handle. 
             "level": config("DJANGO_LOG_LEVEL", "INFO"),
             'propagate': True,
-            # If False, this means that log messages written to django.request
+            # If False, this means that log messages written to django.request 
             # will not be handled by the django logger.
         },
     },
