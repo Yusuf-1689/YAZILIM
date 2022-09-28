362e9c3e1980c7886fe0aef678305ec1fa2d9b79
yusuf <myusufsb252015@gmail.com>
2022-09-28 21:52:14 +0300

    update

--- Django/project/FlightAppsWaggerDebugToolbar/users/serializers.py
+++ Django/project/FlightAppsWaggerDebugToolbar/users/serializers.py
@@ -1,13 +1,10 @@
-
-
-from asyncore import write
-from dataclasses import fields
-from statistics import mode
-from tkinter.ttk import Style
-from rest_framework import serializers,validators
+from rest_framework import serializers, validators
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth.password_validation import validate_password
+
+# User = settings.AUTH_USER_MODEL
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -19,32 +16,34 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[validate_password],
         style={"input_type": "password"}
+
     )
-    
+
     password1 = serializers.CharField(
         write_only=True,
         required=True,
         validators=[validate_password],
         style={"input_type": "password"}
     )
+
     class Meta:
-        model = settings.AUTH_USER_MODEL
+        model = User
         fields = (
             'username',
             'email',
             'first_name',
             'last_name',
             'password',
-            'password1',
+            'password1'
         )
-        
+
     def validate(self, data):
-        if data['password'] != data['password']:
+        if data['password'] != data['password1']:
             raise serializers.ValidationError(
-                {"password: Password didn't match...."}
+                {"password": "Password didn't match..... "}
             )
         return data
-    
+
     def create(self, validated_data):
         password = validated_data.pop("password")
         validated_data.pop('password1')
@@ -52,5 +51,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-        
-        
