d6ea1f775aea282674f32a485a8f65491c07aeea
yusuf <myusufsb252015@gmail.com>
2022-09-29 21:28:27 +0300

    append

--- Django/project/FlightAppsWaggerDebugToolbar/flight/views.py
+++ Django/project/FlightAppsWaggerDebugToolbar/flight/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .serializers import FlightSerializer
 from rest_framework import viewsets
 from .models import Flight, Passenger, Reservation
+from .permissions import IsStafforReadOnly
 
 
 # GET, POST, PUT, DELETE, PATCH
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsStafforReadOnly)
