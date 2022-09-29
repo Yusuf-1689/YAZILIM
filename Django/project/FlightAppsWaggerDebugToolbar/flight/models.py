28362d25fce3ccece1b38a0b98ca4544f817afce
yusuf <myusufsb252015@gmail.com>
2022-09-29 20:27:15 +0300

    models

--- Django/project/FlightAppsWaggerDebugToolbar/flight/models.py
+++ Django/project/FlightAppsWaggerDebugToolbar/flight/models.py
@@ -1,3 +1,19 @@
+
+from operator import mod
 from django.db import models
 
-# Create your models here.
+class Flight(models.Model):
+    flight_number = models.CharField(max_length=20)
+    operating_airlines = models.CharField(max_length=20)
+    departure_city = models.CharField(max_length=20)
+    arrival_city = models.CharField(max_length=20)
+    date_of_departure = models.DateField()
+    etd = models.TimeField()
+    
+    
+    def __str__(self):
+        return f'{self.flight_number} - {self.departure_city} - {self.arrival_city}'
+    
+    
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=20)
