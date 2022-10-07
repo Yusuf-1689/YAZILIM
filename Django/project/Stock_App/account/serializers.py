3d2cc98cef296982723e78ede7526bb37c664fb0
yusuf <myusufsb252015@gmail.com>
2022-10-07 22:47:11 +0300

    update

--- Django/project/Stock_App/account/serializers.py
+++ Django/project/Stock_App/account/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers, validators
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.hashers import make_password
-from dj_rest_auth.serializers import TokenSerializer
+from dj_rest_auth.serializers import TokenSerializer 
 
 
 class RegisterSerializer(serializers.ModelSerializer):
--- Django/project/Stock_App/main/settings.py
+++ Django/project/Stock_App/main/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # drf
     'rest_framework',
     'rest_framework.authtoken',
-    'dj_rest_auth',
+    # 'dj_rest_auth',
 
     # myapp
     'account',
