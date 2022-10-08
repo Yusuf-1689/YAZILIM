ec615065c695b6361d135956a2cfd0e1a47a9804
yusuf <myusufsb252015@gmail.com>
2022-10-08 12:33:02 +0300

    append

--- Django/project/Stock_App/main/urls.py
+++ Django/project/Stock_App/main/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib import admin
+# from django.contrib import admin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("account/", include("account.urls")),
+    # path("account/", include("account.urls")),
 ]
