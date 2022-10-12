d750f13ca4c033acd2681bd2fe681601b2188ef6
yusuf <myusufsb252015@gmail.com>
2022-10-12 23:18:01 +0300

    append

--- Django/project/Stock_App/stock/models.py
+++ Django/project/Stock_App/stock/models.py
@@ -27,3 +27,17 @@ class Product(UpdateCreate):
     brand = models.ForeignKey(
         Brand, on_delete=models.CASCADE, related_name='products')
     stock = models.SmallIntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Firm(UpdateCreate):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
