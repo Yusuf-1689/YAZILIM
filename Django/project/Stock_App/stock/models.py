a1d598d085cc4d3e769f2176113182618ae94f3a
yusuf <myusufsb252015@gmail.com>
2022-10-12 19:21:54 +0300

    models

--- Django/project/Stock_App/stock/models.py
+++ Django/project/Stock_App/stock/models.py
@@ -1,3 +1,29 @@
+from enum import auto
+from operator import mod
 from django.db import models
 
-# Create your models here.
+
+class UpdateCreate(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    
+    
+class Product(UpdateCreate):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='products')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='products')
+    stock = models.SmallIntegerField(blank=True, null=True)
