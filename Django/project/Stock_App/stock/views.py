0ab635ba03ee46af4588085e2feb5cf28a0ae512
yusuf <myusufsb252015@gmail.com>
2022-10-14 00:05:44 +0300

    views

--- Django/project/Stock_App/stock/views.py
+++ Django/project/Stock_App/stock/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import (
+    Category,
+    Brand,
+    Product,
+    Firm,
+    Transaction
+)
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
