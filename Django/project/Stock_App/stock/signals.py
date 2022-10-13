9e5d490c9a774584b9abb028121a9a95ab25d76b
yusuf <myusufsb252015@gmail.com>
2022-10-13 23:45:25 +0300

    append

--- Django/project/Stock_App/stock/signals.py
+++ Django/project/Stock_App/stock/signals.py
@@ -1,11 +1,18 @@
+from itertools import product
 from math import frexp
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import Transaction
+from .models import Transaction, Product
 
 
 @receiver(pre_save, sender=Transaction)
 def calculate_total_price(sender, instance, **kwargs):
     if not instance.price_total:
         instance.price_total = instance.quantity * instance.price
+        
+        
+@receiver(post_save, sender=Transaction)
+def update_stock(sender, instance, **kwargs):
+    product = Product.objects.get(id=instance.product_id)
+    if instance.transection == 1:
         
