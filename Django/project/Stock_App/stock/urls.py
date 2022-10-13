7d5e5f5c957610b7d9184a8ea8b7ad0cc8d9d875
yusuf <myusufsb252015@gmail.com>
2022-10-14 00:19:03 +0300

    urls

--- Django/project/Stock_App/stock/urls.py
+++ Django/project/Stock_App/stock/urls.py
@@ -3,6 +3,12 @@ from .views import (
     CategoryView
 )
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('category', CategoryView)
+
 urlpatterns = [
-     path('category/', CategoryView.as_view())
-]
+    #  path('category/', CategoryView.as_view()),
+     
+] + router.urls
