eb70e91efe10f6e8832681b3f99d99a86d22b953
yusuf <myusufsb252015@gmail.com>
2022-10-16 21:53:22 +0300

    update

--- Django/project/Stock_App/stock/views.py
+++ Django/project/Stock_App/stock/views.py
@@ -1,13 +1,13 @@
-
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import DjangoModelPermissions
+from .permissions import CustomModelPermission
 from .models import (
     Category,
     Brand,
     Product,
     Firm,
-    Transaction,
+    Transaction
 )
 
 from .serializers import (
@@ -16,55 +16,57 @@ from .serializers import (
     ProductSerializer,
     FirmSerializer,
     TransactionSerializer,
-    CategoryProductsSerializer,
-    
+    CategoryProductsSerializer
 )
+
+
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [DjangoModelPermissions]
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
+    permission_classes = [CustomModelPermission]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
     filterset_fields = ['name']
-    
-    
+
     def get_serializer_class(self):
         if self.request.query_params.get('name'):
             return CategoryProductsSerializer
         else:
-            return super().get_serializer_class
-    
+            return super().get_serializer_class()
+
 
 class BrandView(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
-    
-    
+
+
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category', 'brand']
     search_fields = ['name']
-    
+
+
 class FirmView(viewsets.ModelViewSet):
     queryset = Firm.objects.all()
     serializer_class = FirmSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
-    
+
+
 class TransactionView(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['firm', 'transaction', 'product']
     search_fields = ['firm']
-    
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
