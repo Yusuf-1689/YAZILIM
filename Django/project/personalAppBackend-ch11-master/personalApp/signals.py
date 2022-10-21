b8019adcacb31a6174d9ace78f80bf87d1295414
yusuf <myusufsb252015@gmail.com>
2022-10-21 17:12:52 +0300

    signals

---
+++ Django/project/personalAppBackend-ch11-master/personalApp/signals.py
@@ -0,0 +1,19 @@
+
+
+from django.contrib.auth.models import User
+from models import Personal
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=Personal)
+def is_staffed_user(sender,instance,**kwargs):
+    if instance.is_staffed:
+        user = User.objects.get(
+            first_name=instance.first_name,last_name=instance.last_name)
+        if user:
+            user.is_staff = True
+            user.save()
+        else:
+            instance.is_staffed = False
+            instance.save()
+        
