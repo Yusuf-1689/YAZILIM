3f05dba69ee4cd80beec95a92eed0e7f396cb1b6
yusuf <myusufsb252015@gmail.com>
2022-10-22 00:55:59 +0300

    apps

--- Django/project/personalAppBackend-ch11-master/personalApp/apps.py
+++ Django/project/personalAppBackend-ch11-master/personalApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PersonalappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "personalApp"
+
+    def ready(self) -> None:
+        import personalApp.signals
