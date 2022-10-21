9d5045afd5fabf3373e644c0777c05a2dc6f4289
yusuf <myusufsb252015@gmail.com>
2022-10-21 13:56:50 +0300

    views

--- Django/project/personalAppBackend-ch11-master/personalApp/views.py
+++ Django/project/personalAppBackend-ch11-master/personalApp/views.py
@@ -14,3 +14,36 @@ class DepartmentView(generics.ListAPIView):
     queryset=Department.objects.all()
     # permission_classes= [IsAuthenticated]
     
+class DepartmentPersonalView(generics.ListAPIView):
+    serializer_class=DepartmentSerializer
+    queryset=Department.objects.all()
+    # permission_classes= [IsAuthenticated]
+    
+    def get_queryset(self):
+        name = self.kwargs['department']
+        
+        return Department.objects.filter(name__iexact=name)
+    
+
+class PersonalListCreate(ListCreateAPIView):
+    queryset = Personal.objects.all()
+    serializer_class=PersonalSerializer
+    # permission_classes = [IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if self.request.user.is_superuser or self.request.user.is_staff:#sadece ilgili kullanıcı staff veya superuser ise personel create işlemi yapabilir
+            self.perform_create(serializer)
+            data = {
+                "message": f"Personal {serializer.validated_data.get('first_name')} saved successfully!"}
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    def perform_create(self, serializer):
+        serializer.save()
+        
+class 
+     
