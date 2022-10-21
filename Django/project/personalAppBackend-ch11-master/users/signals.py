3c099141f85f0035b42b823c9e2b143e0327eb1f
yusuf <myusufsb252015@gmail.com>
2022-10-22 01:01:10 +0300

    signals

--- Django/project/personalAppBackend-ch11-master/users/signals.py
+++ Django/project/personalAppBackend-ch11-master/users/signals.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
+from .models import Personal
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
-@receiver(post_save, sender=User)
-def create_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+@receiver(post_save,sender=Personal)
+def is_staffed_user(sender,instance,**kwargs):
+    if instance.is_staffed:
+        user= User.objects.get(first_name=instance.first_name,last_name=instance.last_name)
+        if user:
+            user.is_staff = True
+            user.save()
+        else:
+            instance.is_staffed = False
+            instance.save()
