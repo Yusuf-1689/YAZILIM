bfd3f9518d1674e155b8f0f5800fa6422006866e
yusuf <myusufsb252015@gmail.com>
2022-10-21 14:40:37 +0300

    views2

--- Django/project/personalAppBackend-ch11-master/personalApp/views.py
+++ Django/project/personalAppBackend-ch11-master/personalApp/views.py
@@ -7,24 +7,20 @@ from personalApp.serializers import DepartmentPersonalSerializer, DepartmentSeri
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
-
-
+# Create your views here.
 class DepartmentView(generics.ListAPIView):
     serializer_class=DepartmentSerializer
     queryset=Department.objects.all()
-    # permission_classes= [IsAuthenticated]
-    
+    # permission_classes = [IsAuthenticated]
 class DepartmentPersonalView(generics.ListAPIView):
     serializer_class=DepartmentSerializer
     queryset=Department.objects.all()
-    # permission_classes= [IsAuthenticated]
-    
+    # permission_classes = [IsAuthenticated]
     def get_queryset(self):
         name = self.kwargs['department']
-        
-        return Department.objects.filter(name__iexact=name)
-    
-
+        return Department.objects.filter(name__iexact=name)#dinamik url yapmış olduk kullanıcı deparment adına göre istek atabilcek ve sadece ilgili deparment i sergileyebilcek
+        #__iexact küçük büyük aldırış etmeme metodu
+        #bu method filterlamak için kullanılıyor method
 class PersonalListCreate(ListCreateAPIView):
     queryset = Personal.objects.all()
     serializer_class=PersonalSerializer
@@ -44,6 +40,32 @@ class PersonalListCreate(ListCreateAPIView):
             return Response(data, status=status.HTTP_401_UNAUTHORIZED)
     def perform_create(self, serializer):
         serializer.save()
-        
-class 
-     
+class PersonalGetUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Personal.objects.all()
+    serializer_class=PersonalSerializer
+    # permission_classes = [IsAuthenticated]
+    # lookup_field = "id"
+    # default lookup_field is pk olduğu için urlden pk ile yollayınca yazmsak oluyor
+    def put(self, request, *args, **kwargs):
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            return self.update(request, *args, **kwargs)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.request.user.is_superuser:
+            self.perform_destroy(instance)
+            data = {
+                "message": f"Name : {instance.first_name} deleted. "
+            }
+            return Response(data,status=status.HTTP_204_NO_CONTENT,headers=data)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    def perform_destroy(self, instance):
+        instance.delete()
