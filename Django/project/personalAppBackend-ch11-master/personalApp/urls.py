978116a9969f20a7317ceb726c14b892fa3be37e
yusuf <myusufsb252015@gmail.com>
2022-10-21 17:05:35 +0300

    urls

--- Django/project/personalAppBackend-ch11-master/personalApp/urls.py
+++ Django/project/personalAppBackend-ch11-master/personalApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import DepartmentView
+from .views import DepartmentView,DepartmentPersonalView, PersonalGetUpdateDelete, PersonalListCreate
 
 urlpatterns = [
     path('', DepartmentView.as_view()),
+    path('department/<str:department>/', DepartmentPersonalView.as_view()),
+    path('personal/', PersonalListCreate.as_view()),
+    path('personal/<int:pk>',PersonalGetUpdateDelete.as_view()),
 ]
