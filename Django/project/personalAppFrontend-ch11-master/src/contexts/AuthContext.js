3f8d274c0903fc4cb5d832889282130fbd6bb75f
yusuf <myusufsb252015@gmail.com>
2022-10-24 13:47:39 +0300

    authcont

--- Django/project/personalAppFrontend-ch11-master/src/contexts/AuthContext.js
+++ Django/project/personalAppFrontend-ch11-master/src/contexts/AuthContext.js
@@ -6,9 +6,48 @@ export const AutContext = createContext();
 const url = "http://127.0.0.1:8000/"
 
 const AutContextProvider = (props) => {
+  const [currentUser,setCurrentUser] = usestate(false);
+  const [myKey, setMyKey] = useState("")
+
+  const createUser = async (email,password,firstName,lastName,userName)=>{
+    try {
+      const res = await axios.post(`${url}user/auth/register/`,{
+        "username": userName,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "password": password,
+        "password2": password2
+
+      })
+
+      if (res.data.token){
+        setMyKey(res.data.token)
+        setCurrentUser(res.data.userName)
+        sessionStorage.setItem('username',res.data.userName)
+        
+
+      }
+      
+    } catch (error) {
+
+      
+    }
+  }
+
+let value = {
+  createUser,
+  currentUser,
+  myKey,
+
+}
+
+
   return(
     <AutContext.Provider value={}>
       {props.children}
     </AutContext.Provider>
   )
 }
+
+export default AuthContextProvider;
