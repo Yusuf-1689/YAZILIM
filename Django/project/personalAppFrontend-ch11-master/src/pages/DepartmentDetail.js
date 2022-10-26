6a121896c14cc4935b6c5778f4a7420a37e7035f
yusuf <myusufsb252015@gmail.com>
2022-10-26 20:44:41 +0300

    departmentDet

--- Django/project/personalAppFrontend-ch11-master/src/pages/DepartmentDetail.js
+++ Django/project/personalAppFrontend-ch11-master/src/pages/DepartmentDetail.js
@@ -1,6 +1,28 @@
-import React from 'react'
+// import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 const DepartmentDetail = () => {
+  const [data, setData] = useState()
+  const {myKey} = useContext(AuthContext)
+
+  const getDepartments = async () => {
+    const res = await axios.get("http://127.0.1:8000/api/", {
+      headers: {
+        'Authorization': `Token ${myKey}`,
+      }
+    })
+    console.log(res.data)
+  }
+
+  useEffect(() => {
+    getDepartments()
+  
+    
+  }, [])
+  
+
   return (
     <div>DepartmentDetail</div>
   )
