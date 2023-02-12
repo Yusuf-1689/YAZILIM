016b17e54aa74d1b29339f3261070d9372e4bf5e
yusuf <myusufsb252015@gmail.com>
2023-02-12 21:52:19 +0300

    append5

--- React/class-note/lyriks_starter_code/src/pages/Discover.jsx
+++ React/class-note/lyriks_starter_code/src/pages/Discover.jsx
@@ -25,10 +25,11 @@ const Discover = () => {
             </div>
 
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
                     <SongCard
-                        key={song}
+                        key={song.key}
                         song={song}
+                        i={i}
                     >
 
                     </SongCard>
