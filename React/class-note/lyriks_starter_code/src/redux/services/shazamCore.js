ba0b3ad7618c6aa4e8bebd8e4f7be167983d7ba0
yusuf <myusufsb252015@gmail.com>
2023-02-13 13:46:50 +0300

    append3

--- React/class-note/lyriks_starter_code/src/redux/services/shazamCore.js
+++ React/class-note/lyriks_starter_code/src/redux/services/shazamCore.js
@@ -1,2 +1,21 @@
 
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '364165cef0msh1f876093391cdebp1a0796jsna9ef79f7e66d',
+      'X-RapidAPI-Host': ''
+    }
+  };
+  
+  fetch('https://shazam.p.rapidapi.com/charts/track', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+  export const shazamCoreApi = createApi({
+    reducerPath: ' shazamCoreApi',
+  });
