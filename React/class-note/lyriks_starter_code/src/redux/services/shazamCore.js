9037bfb65af4612f57ee00a579e61636afc5455a
yusuf <myusufsb252015@gmail.com>
2023-02-13 15:32:01 +0300

    append4

--- React/class-note/lyriks_starter_code/src/redux/services/shazamCore.js
+++ React/class-note/lyriks_starter_code/src/redux/services/shazamCore.js
@@ -8,14 +8,23 @@ const options = {
       'X-RapidAPI-Key': '364165cef0msh1f876093391cdebp1a0796jsna9ef79f7e66d',
       'X-RapidAPI-Host': ''
     }
-  };
-  
-  fetch('https://shazam.p.rapidapi.com/charts/track', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+};
+fetch('https://shazam.p.rapidapi.com/charts/track', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
+export const shazamCoreApi = createApi({
+  reducerPath: ' shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam.p.rapidapi.com',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '364165cef0msh1f876093391cdebp1a0796jsna9ef79f7e66d');
 
-  export const shazamCoreApi = createApi({
-    reducerPath: ' shazamCoreApi',
-  });
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/charts/tracks'}),
+  }),
+});
