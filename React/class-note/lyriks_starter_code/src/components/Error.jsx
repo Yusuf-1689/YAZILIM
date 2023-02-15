07622f1484e132d23feec2b3ffdb86cfa31bf62a
yusuf <myusufsb252015@gmail.com>
2023-02-15 16:42:54 +0300

    append4

--- React/class-note/lyriks_starter_code/src/components/Error.jsx
+++ React/class-note/lyriks_starter_code/src/components/Error.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+
+
 
 const Error = () => (
-  <div>Error</div>
+  <div>
+    <h1 className="font-bold text-2xl text-white mt-2">Something went wrong. Please try again.</h1>
+  </div>
 );
 
 export default Error;
