3b90fa21a3f3f56a785c97317a8f442774183bed
yusuf <myusufsb252015@gmail.com>
2023-02-24 11:56:39 +0300

    append1

--- React/class-note/lyriks_starter_code/src/components/PlayPause.jsx
+++ React/class-note/lyriks_starter_code/src/components/PlayPause.jsx
@@ -2,9 +2,17 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?. title === song.title ? (
-  <FaPauseCircle />
+  <FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}
+  />
 ) : (
-  <FaPlayCircle />
+  <FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
+  />
 ));
 
 export default PlayPause;
