cc3a54a2bcfb47023492a20a48a85ea03672e503
yusuf <myusufsb252015@gmail.com>
2023-02-26 23:17:46 +0300

    append4

--- React/class-note/lyriks_starter_code/src/components/Sidebar.jsx
+++ React/class-note/lyriks_starter_code/src/components/Sidebar.jsx
@@ -6,6 +6,11 @@ import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from "../assets/constants";
 
+const NavLinks = () => (
+  <div>
+    
+  </div>
+);
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -13,7 +18,7 @@ const Sidebar = () => {
   return (
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
       <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-
+      <NavLinks />
     </div>
   );
 };
