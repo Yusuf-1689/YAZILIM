54c395be5167ca48e960f88866dd0e102950b463
yusuf <myusufsb252015@gmail.com>
2023-02-27 14:08:28 +0300

    append4

--- React/class-note/lyriks_starter_code/src/components/Sidebar.jsx
+++ React/class-note/lyriks_starter_code/src/components/Sidebar.jsx
@@ -35,13 +35,13 @@ const Sidebar = () => {
     <div className="absolute md:hidden block top-6 right-3">
       {mobileMenuOpen ? (
         <RiCloseLine className="w-6 h-6 text-white mr-2" />
-      ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" />}
+      ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}
       
     </div>
 
-    <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden`}>
+    <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden msmoot-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
       <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks />
+      <NavLinks handleClick={() => setMobileMenuOpen (false)}/>
     </div>
    
     </>
