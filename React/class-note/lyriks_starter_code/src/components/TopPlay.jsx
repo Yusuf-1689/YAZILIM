7fb8f0a13b55af645de61dffda01d2d163529fbc
yusuf <myusufsb252015@gmail.com>
2023-03-06 14:19:59 +0300

    append1

--- React/class-note/lyriks_starter_code/src/components/TopPlay.jsx
+++ React/class-note/lyriks_starter_code/src/components/TopPlay.jsx
@@ -37,7 +37,7 @@ const TopPlay = () => {
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
-          <h2>Top Charts</h2>
+          <h2 className="text-white font-ld text-2xl">Top Charts</h2>
           <Link to="/top-charts">
             <p>See More</p>
           </Link>
