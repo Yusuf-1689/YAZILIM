99cedee972b6292b500c44cded2383ceef422835
yusuf <myusufsb252015@gmail.com>
2023-03-13 22:37:04 +0300

    append4

--- React/class-note/lyriks_starter_code/src/components/TopPlay.jsx
+++ React/class-note/lyriks_starter_code/src/components/TopPlay.jsx
@@ -18,6 +18,14 @@ const TopChartChard = ({ song, i }) => (
     </h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-ase font-bold text-gray-300">{song?.title}</p>
+        </Link>
+      </div>
     </div>
   </div>
 );
