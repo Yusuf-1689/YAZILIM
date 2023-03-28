93bdbedee7c945884c6b4b96bdf3f866e0201e6b
yusuf <myusufsb252015@gmail.com>
2023-03-28 15:51:31 +0300

    append1

--- React/class-note/lyriks_starter_code/src/pages/SongDetails.jsx
+++ React/class-note/lyriks_starter_code/src/pages/SongDetails.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 
 const SongDetails = () => {
