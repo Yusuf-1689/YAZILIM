a087385c033e96a377b8137fee728e4eaacea5f6
yusuf <myusufsb252015@gmail.com>
2023-05-14 23:20:29 +0300

    append1

--- React/class-note/website-yt-1/src/components/Button.js
+++ React/class-note/website-yt-1/src/components/Button.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SİZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
     children,
@@ -12,5 +14,20 @@ export const Button = ({
     buttonStyle,
     buttonSize
 }) => {
-   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+   const checkButtonSize = SİZES.includes(buttonSize) ? buttonSize : SİZES[0];
+
+   return (
+    <Link to='/sign-up' className='btn-mobile'>
+        <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+        >
+            {children}
+        </button>
+       
+    </Link>
+   )
 };
