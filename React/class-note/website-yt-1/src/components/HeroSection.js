37fa22e4553c7eeaa995189887c05ce869e5207b
yusuf <myusufsb252015@gmail.com>
2023-05-15 14:40:31 +0300

    append3

---
+++ React/class-note/website-yt-1/src/components/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='hero-container'>
+        <video src='/videos/video-2.mp4' autoPlay loop muted />
+        <h1>ADVENTURE AWAITS</h1>
+    </div>
+  )
+}
+
+export default HeroSection
