14e536fcffe859fec9744939ac0b9014527aaf77
yusuf <myusufsb252015@gmail.com>
2023-05-16 22:14:16 +0300

    herosec

--- React/class-note/website-yt-1/src/components/HeroSection.js
+++ React/class-note/website-yt-1/src/components/HeroSection.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
 
 const HeroSection = () => {
   return (
     <div className='hero-container'>
         <video src='/videos/video-2.mp4' autoPlay loop muted />
         <h1>ADVENTURE AWAITS</h1>
+        <p>what are you waiting for?</p>
+        <div className="hero-btns">
+            <Button 
+              className='btns'
+              buttonStyle='btn--outline'
+              buttonSize='btn--large'
+              >
+                GET STARTED
+            </Button>
+            <Button 
+              className='btns'
+              buttonStyle='btn--primary'
+              buttonSize='btn--large'
+              >
+                WATCH TRAILER <i className='far fa-pla'/>
+            </Button>
+        </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
