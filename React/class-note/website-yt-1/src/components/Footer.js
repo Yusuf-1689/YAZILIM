0d6c1261fb14f0a2a168f8492330106bb84f6bf8
yusuf <myusufsb252015@gmail.com>
2023-05-17 17:50:33 +0300

    footerjs

---
+++ React/class-note/website-yt-1/src/components/Footer.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+            Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+            Yuo can unsubscribe at any time.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default Footer;
