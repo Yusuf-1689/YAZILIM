4020b94a1f98021c481a94ff2cd58b82d204f0dc
yusuf <myusufsb252015@gmail.com>
2023-05-18 13:05:40 +0300

    footercss8

--- React/class-note/website-yt-1/src/components/pages/Home.js
+++ React/class-note/website-yt-1/src/components/pages/Home.js
@@ -3,12 +3,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home () {
     return (
         <>
           <HeroSection />
           <Cards />
+          <Footer />
         </>
     );
 }
---
+++ React/class-note/website-yt-1/src/components/pages/SignUp.js
@@ -0,0 +1,8 @@
+
+
+import React from "react";
+import '../../App.css';
+
+export default function SignUp() {
+    return <h1 className="sig-nup">SIGN UP</h1>
+}
