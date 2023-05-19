97958712904c79b6e89173487fdca6ec37dbd035
yusuf <myusufsb252015@gmail.com>
2023-05-19 23:54:38 +0300

    services

--- React/class-note/website-yt-1/src/components/pages/Services.js
+++ React/class-note/website-yt-1/src/components/pages/Services.js
@@ -1,7 +1,6 @@
-
-import React from "react";
+import React from 'react';
 import '../../App.css';
 
 export default function Services() {
-    return <h1 className="services">SERVICES</h1>
+  return <h1 className='services'>SERVICES</h1>;
 }
