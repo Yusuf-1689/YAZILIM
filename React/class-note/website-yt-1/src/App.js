5f6458dc8e851a58dbd3a60cc913125b39e20843
yusuf <myusufsb252015@gmail.com>
2023-05-26 22:58:35 +0300

    appjs

--- React/class-note/website-yt-1/src/App.js
+++ React/class-note/website-yt-1/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -13,13 +13,12 @@ function App() {
      <Router>
       <Navbar />  
       <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/services'  component={Services} />
-        <Route path='/products'  component={Products} />
-        <Route path='/sign-up'  component={SignUp} />
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
       </Routes>
      </Router>
-     
     </>
   );
 }
