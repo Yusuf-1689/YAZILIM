e3a718b03eb3214f59374c6f737ccd630d9c87bb
yusuf <myusufsb252015@gmail.com>
2023-10-31 23:29:12 +0300

    navbar

---
+++ Çalışma Projelerim/React/Tour-Places/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="#aboutus">About Us</a>
+      <a href="#four you">Four You</a>
+      <a href="#services">Services</a>
+      <a href="#blog">Blog</a>
+      <a href="#vlog">Vlog</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
+
+export default Navbar;
