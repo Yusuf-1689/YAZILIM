3984c677f491f7549df4f5e4872ddd38e30c89e0
yusuf <myusufsb252015@gmail.com>
2023-10-31 09:24:18 +0300

    update

--- Çalışma Projelerim/Js/JS-Weather-App/weather.js
+++ Çalışma Projelerim/Js/JS-Weather-App/weather.js
@@ -3,53 +3,56 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-// localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
+// localStorage.setItem("apiKey", EncryptStringAES("c"));
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    getWeatherDataFromApi();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
 });
 
 // function getWeatherDataFromApi(){}
-const getWeatherDataFromApi = async() =>{
-    // alert("http request gone");
-    // input.value = "";
-    let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
-    // console.log(apikey);
-    let inputVal = input.value;
-    let unitType = "metric";
-    let lang = "tr";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}`;
+const getWeatherDataFromApi = async () => {
+  // alert("http request gone");
+  // input.value = "";
+  let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
+  // console.log(apikey);
+  let inputVal = input.value;
+  let unitType = "metric";
+  let lang = "tr";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}`;
 
-    try {
-        // const response = await fetch.get(url).then(response => response.json());
-        //axios.get(url) == axios(url)
-        const response = await axios(url);
-        const { name, main, sys, weather } = response.data;
-        // console.log(response.data);
-        let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  try {
+    // const response = await fetch.get(url).then(response => response.json());
+    //axios.get(url) == axios(url)
+    const response = await axios(url);
+    const { name, main, sys, weather } = response.data;
+    // console.log(response.data);
+    let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
 
-        //forEach => array + nodeList
-        //map, filter, reduce => array
-        const cityListItems = list.querySelectorAll(".city");
-        const cityListItemsArray = Array.from(cityListItems);
-        if(cityListItemsArray.length > 0){
-            const filteredArray = cityListItemsArray.filter(cityCard => cityCard.querySelector("span").innerText == name);
-            // console.log(cityListItemsArray.length);
-            if(filteredArray.length > 0){
-                msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
-                setTimeout(()=>{
-                    msg.innerText = "";
-                }, 5000);
-                form.reset();
-                return;
-            }
-        }
-        // else{}
-        const createdLi = document.createElement("li");
-        createdLi.classList.add("city");
-        const createdLiInnerHTML = 
-            `<h2 class="city-name" data-name="${name}, ${sys.country}">
+    //forEach => array + nodeList
+    //map, filter, reduce => array
+    const cityListItems = list.querySelectorAll(".city");
+    const cityListItemsArray = Array.from(cityListItems);
+    if (cityListItemsArray.length > 0) {
+      const filteredArray = cityListItemsArray.filter(
+        (cityCard) => cityCard.querySelector("span").innerText == name
+      );
+      // console.log(cityListItemsArray.length);
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
+    }
+    // else{}
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    const createdLiInnerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
                 <span>${name}</span>
                 <sup>${sys.country}</sup>
             </h2>
@@ -58,16 +61,14 @@ const getWeatherDataFromApi = async() =>{
                 <img class="city-icon" src="${iconUrl}">
                 <figcaption>${weather[0].description}</figcaption>
             </figure>`;
-        createdLi.innerHTML = createdLiInnerHTML;
-        //append vs. prepend
-        list.prepend(createdLi);
-
-    } 
-    catch (error) {
-        msg.innerText = error;
-        setTimeout(()=>{
-            msg.innerText = "";
-        }, 5000);
-    }
-    form.reset();
-}
+    createdLi.innerHTML = createdLiInnerHTML;
+    //append vs. prepend
+    list.prepend(createdLi);
+  } catch (error) {
+    msg.innerText = error;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
+  }
+  form.reset();
+};
