7b3bed037c8640ab1677cdd30168ae51f7c8c850
yusuf <myusufsb252015@gmail.com>
2025-01-19 11:29:56 +0300

    1

--- React/class-note/rc20_redux-intro/src/redux/actions/counterActions.jsx
+++
@@ -1,8 +0,0 @@
-import { DECREMENT, INCREMENT, RESET } from '../types/counterTypes';
-
-export const increment = () => {
-  return { type: INCREMENT };
-};
-
-export const decrement = () => ({ type: DECREMENT });
-export const reset = () => ({ type: RESET });
